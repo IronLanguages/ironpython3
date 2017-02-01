cdc27331c16ad67281a8ec1c6a0834e0f1334103
Stéphane Lozier <slozier@users.noreply.github.com>
2017-02-01 13:56:44 -0500

    Add missing module

---
+++ Src/IronPython.Modules/_string.cs
@@ -0,0 +1,8 @@
+﻿using IronPython.Runtime;
+
+[assembly: PythonModule("_string", typeof(IronPython.Modules.PythonString))]
+namespace IronPython.Modules {
+    public static class PythonString {
+        // TODO: fill this up
+    }
+}

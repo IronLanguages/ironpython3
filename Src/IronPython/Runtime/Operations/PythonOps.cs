e2e23b7c4d64fd37328715e62a23aac2a3530fa1
Alex Earl <slide.o.mix@gmail.com>
2017-02-05 15:33:02 -0700

    Make all classes inherit from object (#188)
    * Make all classes inherit from object
    
    * Fixed location of adding object to bases if not there
    
    * Fix the alternate return point

--- Src/IronPython/Runtime/Operations/PythonOps.cs
+++ Src/IronPython/Runtime/Operations/PythonOps.cs
@@ -1348,6 +1348,10 @@ namespace IronPython.Runtime.Operations {
 
             object metaclass;
             if (!TryGetMetaclass(context, tupleBases, vars, out metaclass)) {
+                // this makes sure that object is a base
+                if(tupleBases.Count == 0) {
+                    tupleBases = PythonTuple.MakeTuple(DynamicHelpers.GetPythonTypeFromType(typeof(object)));
+                }
                 return PythonType.__new__(context, TypeCache.PythonType, name, tupleBases, vars, selfNames);
             }
 
@@ -1355,7 +1359,7 @@ namespace IronPython.Runtime.Operations {
 
             if (metaclass is PythonType) {
                 classdict = CallPrepare(context, (PythonType)metaclass, name, tupleBases, vars);
-            }
+            }            
 
             // eg:
             // def foo(*args): print args            
@@ -1364,7 +1368,7 @@ namespace IronPython.Runtime.Operations {
             // calls our function...
             PythonContext pc = PythonContext.GetContext(context);
 
-            return pc.MetaClassCallSite.Target(
+            object obj = pc.MetaClassCallSite.Target(
                 pc.MetaClassCallSite,
                 context,
                 metaclass,
@@ -1372,6 +1376,13 @@ namespace IronPython.Runtime.Operations {
                 tupleBases,
                 classdict
             );
+
+            PythonType newType = obj as PythonType;
+            if(newType != null && newType.BaseTypes.Count == 0) {
+                newType.BaseTypes.Add(DynamicHelpers.GetPythonTypeFromType(typeof(object)));
+            }
+
+            return obj;
         }
 
         /// <summary>

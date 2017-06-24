455349f51da2d471f7de2d9856d438fa4bbce7be
Stéphane Lozier <slozier@users.noreply.github.com>
2017-06-24 14:58:08 -0400

    IronLanguages/ironpython2#157

--- Src/IronPython/Runtime/SetStorage.cs
+++ Src/IronPython/Runtime/SetStorage.cs
@@ -170,13 +170,17 @@ namespace IronPython.Runtime {
             Debug.Assert(buckets != null);
             Debug.Assert(item != null);
 
+            int add_index = -1;
             for (int index = hashCode & (buckets.Length - 1); ; ProbeNext(buckets, ref index)) {
                 Bucket bucket = buckets[index];
-                if (bucket.Item == null || bucket.Item == Removed) {
+                if (bucket.Item == null) {
                     version++;
+                    if (add_index != -1) index = add_index;
                     buckets[index].HashCode = hashCode;
                     buckets[index].Item = item;
                     return true;
+                } else if (bucket.Item == Removed && add_index == -1) {
+                    add_index = index;
                 } else if (bucket.HashCode == hashCode && eqFunc(item, bucket.Item)) {
                     return false;
                 }

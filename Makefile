e73f676ab2214d88850d88d41c56ced51e3949b3
Alex Earl <slide.o.mix@gmail.com>
2017-07-12 08:49:58 -0700

    Align build setup with ironpython2

--- Makefile
+++ Makefile
@@ -1,22 +1,43 @@
-.PHONY: debug release test stage package clean test-smoke
-
-debug:
-	@xbuild Build.proj /t:Build /p:Mono=true /p:BuildFlavour=Debug /p:Platform="Any CPU" /verbosity:minimal /nologo
+.PHONY: debug release test stage package clean test-smoke test-smoke-debug test-ironpython test-ironpython-debug test-cpython test-cpython-debug test-all test-all-debug
 
 release:
-	@xbuild Build.proj /t:Build /p:Mono=true /p:BuildFlavour=Release /p:Platform="Any CPU" /verbosity:minimal /nologo
+	@msbuild Build.proj /t:Build /p:Mono=true /p:BuildFlavour=Release /p:Platform="Any CPU" /verbosity:minimal /nologo
+	cp Src/DLR/bin/Release/rowantest.*.dll bin/Release/
+
+debug:
+	@msbuild Build.proj /t:Build /p:Mono=true /p:BuildFlavour=Debug /p:Platform="Any CPU" /verbosity:minimal /nologo
+	cp Src/DLR/bin/Debug/rowantest.*.dll bin/Debug/
 
 stage:
-	@xbuild Build.proj /t:Stage /p:Mono=true;BuildFlavour=Release /verbosity:minimal /nologo
+	@msbuild Build.proj /t:Stage /p:Mono=true /p:BuildFlavour=Release /verbosity:minimal /nologo
 
 package:
-	@xbuild Build.proj /t:Package /p:Mono=true;BuildFlavour=Release /verbosity:minimal /nologo
+	@msbuild Build.proj /t:Package /p:Mono=true /p:BuildFlavour=Release /verbosity:minimal /nologo
 
 clean:
-	@xbuild Build.proj /t:Clean /p:Mono=true /verbosity:minimal /nologo
+	@msbuild Build.proj /t:Clean /p:Mono=true /verbosity:minimal /nologo
+
+test-ironpython:
+	(cd bin/Release && mono ./IronPythonTest.exe --labels=All --where:Category==IronPython --result:ironpython-net45-release-result.xml) || true
+
+test-ironpython-debug:
+	(cd bin/Debug && mono ./IronPythonTest.exe --labels=All --where:Category==IronPython --result:ironpython-net45-debug-result.xml) || true
+
+test-cpython:
+	(cd bin/Release && mono ./IronPythonTest.exe --labels=All --where:"Category==StandardCPython || Category==AllCPython" --result:cpython-net45-release-result.xml) || true
+
+test-cpython-debug:
+	(cd bin/Debug && mono ./IronPythonTest.exe --labels=All --where:"Category==StandardCPython || Category==AllCPython" --result:cpython-net45-debug-result.xml) || true
+
 
 test-smoke:
-	(cd bin/Debug && mono ./IronPythonTest.exe --labels=All --where:Category==StandardCPython --result=smoke-result-net45.xml) || true
+	(cd bin/Release && mono ./IronPythonTest.exe --labels=All --where:Category==StandardCPython --result=smoke-net45-release-result.xml) || true
 
-test-smoke-release:
-	(cd bin/Release && mono ./IronPythonTest.exe --labels=All --where:Category==StandardCPython --result=smoke-result-net45.xml) || true
+test-smoke-debug:
+	(cd bin/Debug && mono ./IronPythonTest.exe --labels=All --where:Category==StandardCPython --result=smoke-net45-debug-result.xml) || true
+
+test-all:
+	(cd bin/Release && mono ./IronPythonTest.exe --labels=All --result=all-net45-release-result.xml) || true
+
+test-all-debug:
+	(cd bin/Debug && mono ./IronPythonTest.exe --labels=All --result=all-result-debug-net45.xml) || true
--- make.cmd
+++ make.cmd
@@ -7,7 +7,7 @@ if "%1"=="" (goto :default) else (goto :%1)
 goto :exit
 
 :default
-goto :debug
+goto :release
 
 :debug
 set _target=Build
@@ -61,55 +61,55 @@ echo No target 'test'. Try 'test-smoke', 'test-ironpython', 'test-cpython', or '
 goto :exit
 
 :test-smoke
-pushd bin\Debug
-IronPythonTest.exe --labels=All --where:Category==StandardCPython --result:smoke-result-net45.xml
+pushd bin\Release
+IronPythonTest.exe --labels=All --where:Category==StandardCPython --result:smoke-net45-release-result.xml
 popd
 goto :exit
 
-:test-smoke-release
-pushd bin\Release
-IronPythonTest.exe --labels=All --where:Category==StandardCPython --result:smoke-result-net45.xml
+:test-smoke-debug
+pushd bin\Debug
+IronPythonTest.exe --labels=All --where:Category==StandardCPython --result:smoke-net45-debug-result.xml
 popd
 goto :exit
 
 :test-ironpython
-pushd bin\Debug
-IronPythonTest.exe --labels=All --where:Category==IronPython --result:ironpython-result.xml
+pushd bin\Release
+IronPythonTest.exe --labels=All --where:Category==IronPython --result:ironpython-net45-release-result.xml
 popd
 goto :exit
 
-:test-ironpython-release
+:test-ironpython-debug
 pushd bin\Release
-IronPythonTest.exe --labels=All --where:Category==IronPython --result:ironpython-result.xml
+IronPythonTest.exe --labels=All --where:Category==IronPython --result:ironpython-net45-debug-result.xml
 popd
 goto :exit
 
 :test-cpython
-pushd bin\Debug
-IronPythonTest.exe --labels=All --where:"Category==StandardCPython || Category==AllCPython" --result:cpython-result.xml
+pushd bin\Release
+IronPythonTest.exe --labels=All --where:"Category==StandardCPython || Category==AllCPython" --result:cpython-net45-release-result.xml
 popd
 goto :exit
 
-:test-cpython-release
-pushd bin\Release
-IronPythonTest.exe --labels=All --where:"Category==StandardCPython || Category==AllCPython" --result:cpython-result.xml
+:test-cpython-debug
+pushd bin\Debug
+IronPythonTest.exe --labels=All --where:"Category==StandardCPython || Category==AllCPython" --result:cpython-net45-debug-result.xml
 popd
 goto :exit
 
 :test-all
-pushd bin\Debug
-IronPythonTest.exe --labels=All --result:all-result.xml
+pushd bin\Release
+IronPythonTest.exe --labels=All --result:all-net45-release-result.xml
 popd
 goto :exit
 
-:test-all-release
-pushd bin\Release
-IronPythonTest.exe --labels=All --result:all-result.xml
+:test-all-debug
+pushd bin\Debug
+IronPythonTest.exe --labels=All --result:all-net45-debug-result.xml
 popd
 goto :exit
 
 :test-custom
-pushd bin\Debug
+pushd bin\Release
 shift
 IronPythonTest.exe --labels=All --result:custom-result.xml %1 %2 %3 %4 %5 %6 %7 %8 %9
 popd
@@ -125,8 +125,18 @@ msbuild /t:DistClean /p:BuildFlavour=Release /verbosity:minimal /nologo /p:Platf
 msbuild /t:DistClean /p:BuildFlavour=Debug /verbosity:minimal /nologo /p:Platform="Any CPU"
 goto :main
 
+:ngen
+%SystemRoot%\Microsoft.NET\Framework\v4.0.30319\ngen.exe install bin\Release\ipy.exe
+goto :exit
+
 :main
 msbuild Build.proj /t:%_target% /p:BuildFlavour=%_flavour% /verbosity:minimal /nologo /p:Platform="Any CPU"
+if "%_target%" == "Build" (
+    echo "Copying %_flavour% test dlls"
+    xcopy /y /q Src\DLR\bin\%_flavour%\*.dll bin\%_flavour%\
+    echo "Copying v4%_flavour% test dlls"
+    xcopy /y /q Src\DLR\bin\v4%_flavour%\*.dll bin\v4%_flavour%\
+)
 goto :exit
 
 :exit

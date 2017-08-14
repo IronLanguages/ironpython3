4e0abc57352aac29950567a7ccf44dc3a6c0125f
gpetrou <gpetrou@actify.com>
2017-08-14 15:35:20 +0100

    Use FindRoot in IronPythonCaseGenerator

--- Src/IronPythonTest/Cases/IronPythonCases.cs
+++ Src/IronPythonTest/Cases/IronPythonCases.cs
@@ -29,7 +29,7 @@ namespace IronPythonTest.Cases {
 
     class IronPythonCaseGenerator : CommonCaseGenerator<IronPythonCases> {
         protected override IEnumerable<TestInfo> GetTests() {
-            return Directory.EnumerateFiles(Path.Combine("..", "..", "Tests"), "test_*.py", SearchOption.AllDirectories)
+            return Directory.EnumerateFiles(Path.Combine(CaseExecuter.FindRoot(), "Tests"), "test_*.py", SearchOption.AllDirectories)
                 .Select(file => new TestInfo(Path.GetFullPath(file), this.manifest))
                 .OrderBy(testcase => testcase.Name);
         }

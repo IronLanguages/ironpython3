b4e806a799958c9ac2c5145f2a2c70cbb2a6cb0b
Stéphane Lozier <slozier@users.noreply.github.com>
2018-08-18 15:12:54 -0400

    IronLanguages/ironpython2#484

--- Src/IronPython/Modules/_ast.cs
+++ Src/IronPython/Modules/_ast.cs
@@ -60,10 +60,10 @@ namespace IronPython.Modules
             }
         }
 
-        internal static PythonAst ConvertToPythonAst(CodeContext codeContext, AST source ) {
+        internal static PythonAst ConvertToPythonAst(CodeContext codeContext, AST source, string filename) {
             Statement stmt;
             PythonCompilerOptions options = new PythonCompilerOptions(ModuleOptions.ExecOrEvalCode);
-            SourceUnit unit = new SourceUnit(codeContext.LanguageContext, NullTextContentProvider.Null, "", SourceCodeKind.AutoDetect);
+            SourceUnit unit = new SourceUnit(codeContext.LanguageContext, NullTextContentProvider.Null, filename, SourceCodeKind.AutoDetect);
             CompilerContext compilerContext = new CompilerContext(unit, options, ErrorSink.Default);
             bool printExpression = false;
 

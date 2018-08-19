22b1dab27724ae942bf363307460c634aec2e3bf
slozier <slozier@users.noreply.github.com>
2018-08-19 15:09:21 -0400

    Update copyright notices (#399)
    https://github.com/IronLanguages/ironpython2/pull/438

--- Src/IronPython/Lib/iptest/test_env.py
+++ Src/IronPython/Lib/iptest/test_env.py
@@ -1,17 +1,6 @@
-#####################################################################################
-#
-#  Copyright (c) Microsoft Corporation. All rights reserved.
-#
-# This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
-# copy of the license can be found in the License.html file at the root of this distribution. If 
-# you cannot locate the  Apache License, Version 2.0, please send an email to 
-# ironpy@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
-# by the terms of the Apache License, Version 2.0.
-#
-# You must not remove this notice, or any other, from this software.
-#
-#
-#####################################################################################
+# Licensed to the .NET Foundation under one or more agreements.
+# The .NET Foundation licenses this file to you under the Apache 2.0 License.
+# See the LICENSE file in the project root for more information.
 
 import sys, os
 #from iptest.util import get_env_var, get_temp_dir

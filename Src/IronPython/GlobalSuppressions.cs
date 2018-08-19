39caffabab85d9e25ea6c16312f3b0900efd9b8c
Stéphane Lozier <slozier@users.noreply.github.com>
2018-08-19 14:49:04 -0400

    Update copyright notices

--- Src/IronPython/GlobalSuppressions.cs
+++ Src/IronPython/GlobalSuppressions.cs
@@ -1,17 +1,6 @@
-/* ****************************************************************************
- *
- * Copyright (c) Microsoft Corporation. 
- *
- * This source code is subject to terms and conditions of the Apache License, Version 2.0. A 
- * copy of the license can be found in the License.html file at the root of this distribution. If 
- * you cannot locate the  Apache License, Version 2.0, please send an email to 
- * dlr@microsoft.com. By using this source code in any fashion, you are agreeing to be bound 
- * by the terms of the Apache License, Version 2.0.
- *
- * You must not remove this notice, or any other, from this software.
- *
- *
- * ***************************************************************************/
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1007:UseGenericsWhereAppropriate", Scope = "member", Target = "IronPython.Runtime.Operations.Ops.TryGetAttr(System.Object,IronPython.Runtime.string,System.Object&):System.Boolean")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1007:UseGenericsWhereAppropriate", Scope = "member", Target = "IronPython.Runtime.Generator+NextTarget.Invoke(IronPython.Runtime.Generator,System.Object&):System.Boolean")]

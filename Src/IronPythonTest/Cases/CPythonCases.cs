ee919ebee15aa286333e872d59718a34e59b04e4
Stéphane Lozier <slozier@users.noreply.github.com>
2018-08-20 09:38:37 -0400

    IronLanguages/ironpython2#441, IronLanguages/ironpython2#474 and IronLanguages/ironpython2#488

--- Src/IronPythonTest/Cases/CPythonCases.cs
+++ Src/IronPythonTest/Cases/CPythonCases.cs
@@ -24,6 +24,7 @@ namespace IronPythonTest.Cases {
         [Test, TestCaseSource(typeof(StandardCPythonCaseGenerator))]
         public int StandardCPythonTests(TestInfo testcase) {
             try {
+                TestContext.Progress.WriteLine(testcase.Name); // should be printed immediately
                 return executor.RunTest(testcase);
             } catch (Exception e) {
                 Assert.Fail(executor.FormatException(e));
@@ -89,26 +90,29 @@ namespace IronPythonTest.Cases {
         };
 
         protected override IEnumerable<TestInfo> GetTests() {
-            string stdlib = Path.Combine(CaseExecuter.FindRoot(), "Src", "StdLib", "Lib", "test");
-            return STDTESTS.Select(test => new TestInfo(Path.GetFullPath(Path.Combine(stdlib, test) + ".py"), this.manifest));
+            var testDir = Path.Combine("Src", "StdLib", "Lib", "test");
+            var fullPath = Path.Combine(CaseExecuter.FindRoot(), testDir);
+            return STDTESTS.Select(test => new TestInfo(Path.GetFullPath(Path.Combine(fullPath, test) + ".py"), category, testDir, this.manifest));
         }
     }
 
     class AllCPythonCaseGenerator : CommonCaseGenerator<AllCPythonCases> {
         protected override IEnumerable<TestInfo> GetTests() {
-            string stdlib = Path.Combine(CaseExecuter.FindRoot(), "Src", "StdLib", "Lib", "test");
-            return Directory.GetFiles(stdlib, "test_*.py", SearchOption.AllDirectories)
+            var testDir = Path.Combine("Src", "StdLib", "Lib", "test");
+            var fullPath = Path.Combine(CaseExecuter.FindRoot(), testDir);
+            return Directory.EnumerateFiles(fullPath, "test_*.py", SearchOption.AllDirectories)
                 .Where(file => !StandardCPythonCaseGenerator.STDTESTS.Contains(Path.GetFileNameWithoutExtension(file)))
-                .Select(file => new TestInfo(Path.GetFullPath(file), this.manifest))
+                .Select(file => new TestInfo(Path.GetFullPath(file), category, testDir, this.manifest))
                 .OrderBy(testcase => testcase.Name);
         }
     }
 
     class CTypesCPythonCaseGenerator : CommonCaseGenerator<CTypesCPythonCases> {
         protected override IEnumerable<TestInfo> GetTests() {
-            string stdlib = Path.Combine(CaseExecuter.FindRoot(), "Src", "StdLib", "Lib", "ctypes", "test");
-            return Directory.GetFiles(stdlib, "test_*.py", SearchOption.AllDirectories)
-                .Select(file => new TestInfo(Path.GetFullPath(file), this.manifest))
+            var testDir = Path.Combine("Src", "StdLib", "Lib", "ctypes", "test");
+            var fullPath = Path.Combine(CaseExecuter.FindRoot(), testDir);
+            return Directory.EnumerateFiles(fullPath, "test_*.py", SearchOption.AllDirectories)
+                .Select(file => new TestInfo(Path.GetFullPath(file), category, testDir, this.manifest))
                 .OrderBy(testcase => testcase.Name);
         }
     }

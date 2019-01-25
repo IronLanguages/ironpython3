df98a868874c52db1087ad14cbd55abc6a1788f4
Stéphane Lozier <slozier@users.noreply.github.com>
2019-01-24 20:10:36 -0500

    IronLanguages/ironpython2#565

---
+++ Src/IronPython.Modules/_sysconfigdata.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+using IronPython.Runtime;
+
+[assembly: PythonModule("_sysconfigdata", typeof(IronPython.Modules._sysconfigdata), PlatformsAttribute.PlatformFamily.Unix)]
+namespace IronPython.Modules {
+    public static class _sysconfigdata {
+        public static PythonDictionary build_time_vars = new PythonDictionary();
+    }
+}

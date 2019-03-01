2d2d40560f23b79b05aca4ab0e16992e758c03b1
Pavel Koneski <pavel.koneski@gmail.com>
2019-03-01 05:48:20 -0800

    Bugfix in  PythonAsciiEncoding (#559)

--- Src/IronPython/Runtime/PythonAsciiEncoding.cs
+++ Src/IronPython/Runtime/PythonAsciiEncoding.cs
@@ -194,7 +194,7 @@ namespace IronPython.Runtime {
         private int _index;
 
         public override bool Fallback(char charUnknownHigh, char charUnknownLow, int index) {
-            throw PythonOps.UnicodeEncodeError("'ascii' codec can't encode character '\\u{0:X}{1:04X}' in position {1}: ordinal not in range(128)", (int)charUnknownHigh, (int)charUnknownLow, index);
+            throw PythonOps.UnicodeEncodeError("'ascii' codec can't encode character '\\u{0:X}{1:04X}' in position {2}: ordinal not in range(128)", (int)charUnknownHigh, (int)charUnknownLow, index);
         }
 
         public override bool Fallback(char charUnknown, int index) {
@@ -207,6 +207,9 @@ namespace IronPython.Runtime {
         }
         
         public override char GetNextChar() {
+            if (_index == _buffer.Count) {
+                return char.MinValue;
+            }
             return _buffer[_index++];
         }
 

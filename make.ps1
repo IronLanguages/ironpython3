f7ab250d92c104f496dc253ff1af0b3bf6ed5471
Stéphane Lozier <slozier@users.noreply.github.com>
2019-03-13 21:40:50 -0400

    IronLanguages/ironpython2#606

--- make.ps1
+++ make.ps1
@@ -21,7 +21,7 @@ if(!$global:isUnix) {
     if([System.IO.File]::Exists($_VSWHERE)) {
         $_VSINSTPATH = & "$_VSWHERE" -latest -requires Microsoft.Component.MSBuild -property installationPath
     } else {
-        Write-Error "Visual Studio 2017 15.2 or later is required"
+        Write-Error "Visual Studio 2017 15.9 or later is required"
         Exit 1
     }
 
@@ -31,10 +31,16 @@ if(!$global:isUnix) {
     }
 
     if([System.IO.File]::Exists([System.IO.Path]::Combine($_VSINSTPATH, 'MSBuild\Current\Bin\MSBuild.exe'))) {
-        $env:PATH = [String]::Join(';', $env:PATH, [System.IO.Path]::Combine($_VSINSTPATH, 'MSBuild\Current\Bin'))
+        $_MSBUILDPATH = [System.IO.Path]::Combine($_VSINSTPATH, 'MSBuild\Current\Bin\')
+        if ($env:PATH -notcontains $_MSBUILDPATH) {
+            $env:PATH = [String]::Join(';', $env:PATH, $_MSBUILDPATH)
+        }
     }
     elseif([System.IO.File]::Exists([System.IO.Path]::Combine($_VSINSTPATH, 'MSBuild\15.0\Bin\MSBuild.exe'))) {
-        $env:PATH = [String]::Join(';', $env:PATH, [System.IO.Path]::Combine($_VSINSTPATH, 'MSBuild\15.0\Bin'))
+        $_MSBUILDPATH = [System.IO.Path]::Combine($_VSINSTPATH, 'MSBuild\15.0\Bin\')
+        if ($env:PATH -notcontains $_MSBUILDPATH) {
+            $env:PATH = [String]::Join(';', $env:PATH, $_MSBUILDPATH)
+        }
     }
 }
 

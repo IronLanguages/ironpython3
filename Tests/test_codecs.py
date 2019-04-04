ce5654044d568e9f89ee7256a3c72dd06a2a5b58
Pavel Koneski <pavel.koneski@gmail.com>
2019-04-03 18:26:42 -0700

    Test interop DecoderFallbackException and UnicodeDecodeError

--- Tests/test_codecs.py
+++ Tests/test_codecs.py
@@ -99,4 +99,93 @@ class CodecsTest(unittest.TestCase):
         self.assertEqual("abć".encode('utf_32_be'), b"\x00\x00\x00a\x00\x00\x00b\x00\x00\x01\x07")
         self.assertEqual(b"\x00\x00\x00a\x00\x00\x00b\x00\x00\x01\x07".decode('utf_32_be'), "abć")
 
+    def test_interop_ascii_decode_exeption(self):
+        def check_error(encoding, name):
+            with self.assertRaises(UnicodeDecodeError) as ude:
+                b"abc\xffxyz".decode(encoding)
+            self.assertEqual(ude.exception.encoding, name)
+            self.assertEqual(ude.exception.object, b"abc\xffxyz")
+            self.assertEqual(ude.exception.start, 3)
+            self.assertEqual(ude.exception.end, 4)
+
+        check_error(System.Text.ASCIIEncoding(), 'us-ascii')
+        check_error(System.Text.Encoding.ASCII, 'us-ascii')
+        check_error('ascii', 'ascii')
+
+    def test_interop_utf8_decode_exeption(self):
+        def check_error(encoding, name):
+            with self.assertRaises(UnicodeDecodeError) as ude:
+                # broken input (� is 0xff): "abć�ẋyz"
+                # does not work with 'strict' due to UTF-8 bug https://github.com/dotnet/corefx/issues/29898
+                b"ab\xc4\x87\xff\xe1\xba\x8byz".decode(encoding,'surrogatepass')
+            self.assertEqual(ude.exception.encoding, name)
+            self.assertEqual(ude.exception.object, b"ab\xc4\x87\xff\xe1\xba\x8byz")
+            self.assertEqual(ude.exception.start, 4)
+            self.assertEqual(ude.exception.end, 5)
+
+        check_error(System.Text.UTF8Encoding(encoderShouldEmitUTF8Identifier=False, throwOnInvalidBytes=True), 'utf-8')
+        check_error(System.Text.UTF8Encoding(encoderShouldEmitUTF8Identifier=True, throwOnInvalidBytes=True), 'utf-8')
+        check_error(System.Text.Encoding.UTF8, 'utf-8')
+        check_error('utf-8', 'utf-8')
+
+    def test_interop_utf8bom_decode_exeption(self):
+        def check_error(encoding, name):
+            with self.assertRaises(UnicodeDecodeError) as ude:
+                # broken input (� is 0xff): BOM + "abć�ẋyz"
+                # does not work with 'strict' due to UTF-8 bug https://github.com/dotnet/corefx/issues/29898
+                b"\xef\xbb\xbfab\xc4\x87\xff\xe1\xba\x8byz".decode(encoding,'surrogatepass')
+            self.assertEqual(ude.exception.encoding, name)
+            # regular utf-8 retains BOM if present
+            self.assertEqual(ude.exception.object, b"\xef\xbb\xbfab\xc4\x87\xff\xe1\xba\x8byz")
+            self.assertEqual(ude.exception.start, 7)
+            self.assertEqual(ude.exception.end, 8)
+
+        check_error(System.Text.UTF8Encoding(encoderShouldEmitUTF8Identifier=False, throwOnInvalidBytes=True), 'utf-8')
+        check_error('utf-8', 'utf-8')
+
+    def test_interop_utf8sigbom_decode_exeption(self):
+        def check_error(encoding, name):
+            with self.assertRaises(UnicodeDecodeError) as ude:
+                # broken input (� is 0xff): BOM_UTF8 + "abć�ẋyz"
+                # does not work with 'strict' due to UTF-8 bug https://github.com/dotnet/corefx/issues/29898
+                b"\xef\xbb\xbfab\xc4\x87\xff\xe1\xba\x8byz".decode(encoding,'surrogatepass')
+            self.assertEqual(ude.exception.encoding, name)
+            # utf-8 with signature skips BOM
+            self.assertEqual(ude.exception.object, b"ab\xc4\x87\xff\xe1\xba\x8byz")
+            self.assertEqual(ude.exception.start, 4)
+            self.assertEqual(ude.exception.end, 5)
+
+        check_error(System.Text.UTF8Encoding(encoderShouldEmitUTF8Identifier=True, throwOnInvalidBytes=True), 'utf-8')
+        check_error(System.Text.Encoding.UTF8, 'utf-8')
+        check_error('utf-8-sig', 'utf-8-sig') # TODO: CPython uses 'utf-8' as encoding name in UnicodeDecodeError
+
+    def test_interop_utf16_decode_exception(self):
+        def check_error(encoding, name):
+            with self.assertRaises(UnicodeDecodeError) as ude:
+                # broken input (� is lone surrogate 0xdddd): BOM_UTF16_LE + "abć�ẋyz"
+                b'\xff\xfea\x00b\x00\x07\x01\xdd\xdd\x8b\x1ey\x00z\x00'.decode(encoding,'strict')
+            self.assertEqual(ude.exception.encoding, name)
+            # regular utf-16 skips BOM
+            # NOTE: CPython is not consistent in this behavor, possibly a CPython bug
+            self.assertEqual(ude.exception.object, b'a\x00b\x00\x07\x01\xdd\xdd\x8b\x1ey\x00z\x00')
+            self.assertEqual(ude.exception.start, 6)
+            self.assertEqual(ude.exception.end, 8)
+
+        check_error(System.Text.UnicodeEncoding(bigEndian=False, byteOrderMark=True, throwOnInvalidBytes=True), 'utf-16LE')
+        check_error('utf-16', 'utf-16') # TODO: should be 'utf-16LE', CPython uses 'utf-16-le' here
+
+    def test_interop_utf16le_decode_exception(self):
+        def check_error(encoding, name):
+            with self.assertRaises(UnicodeDecodeError) as ude:
+                # broken input (� is lone surrogate 0xdddd): BOM_UTF16_LE + "abć�ẋyz"
+                b'\xff\xfea\x00b\x00\x07\x01\xdd\xdd\x8b\x1ey\x00z\x00'.decode(encoding,'strict')
+            self.assertEqual(ude.exception.encoding, name)
+            # utf-16LE treats BOM as a regular character
+            self.assertEqual(ude.exception.object, b'\xff\xfea\x00b\x00\x07\x01\xdd\xdd\x8b\x1ey\x00z\x00')
+            self.assertEqual(ude.exception.start, 8)
+            self.assertEqual(ude.exception.end, 10)
+
+        check_error(System.Text.UnicodeEncoding(bigEndian=False, byteOrderMark=False, throwOnInvalidBytes=True), 'utf-16LE')
+        check_error('utf-16LE', 'utf-16LE')
+
 run_test(__name__)

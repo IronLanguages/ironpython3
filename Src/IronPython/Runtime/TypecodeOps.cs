73d62ff5134c2556727db2e35eeacb366b6053b5
Graham McKnight <graham.mcknight2@gmail.com>
2019-05-22 14:36:56 -0700

    Implement cast() on memoryview (#617)
    * Implement cast() on memoryview
    
    Refactor MemoryView to refer to the beginning and ending byte of the
    underlying buffer rather than the index of the underlying buffer.
    Implement cast() to change the shape and format of the memoryview. Add
    tests in IronPython.test_memoryview for this behavior.
    
    * Initialize properties immediately when a memoryview is created. Use a
    try structure for typecode information in TypecodeOps.
    
    * Add a fast path for getting/setting elements when the memoryview matches
    the underlying buffer.
    
    * Make __len__ return the length of the first dimension. Move the check
    for invalid typecodes before checking for conversions between nonbyte
    typecodes.

---
+++ Src/IronPython/Runtime/TypecodeOps.cs
@@ -0,0 +1,127 @@
+﻿using IronPython.Runtime.Operations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IronPython.Runtime {
+    class TypecodeOps {
+
+        public static bool TryGetTypecodeWidth(string typecode, out int width) {
+            switch (typecode) {
+                case "c": // char
+                case "b": // signed byte
+                case "B": // unsigned byte
+                case "x": // pad byte
+                case "s": // null-terminated string
+                case "p": // Pascal string
+                    width = 1;
+                    return true;
+                case "u": // unicode char
+                case "h": // signed short
+                case "H": // unsigned short
+                    width = 2;
+                    return true;
+                case "i": // signed int
+                case "I": // unsigned int
+                case "l": // signed long
+                case "L": // unsigned long
+                case "f": // float
+                    width = 4;
+                    return true;
+                case "P": // pointer
+                    width = IntPtr.Size;
+                    return true;
+                case "q": // signed long long
+                case "Q": // unsigned long long
+                case "d": // double
+                    width = 8;
+                    return true;
+                default:
+                    width = 0;
+                    return false;
+            }
+        }
+
+        public static bool TryGetFromBytes(string typecode, byte[] bytes, int offset, out object result) {
+            switch (typecode) {
+                case "c":
+                    result = (char)bytes[offset];
+                    return true;
+                case "b":
+                    result = (sbyte)bytes[offset];
+                    return true;
+                case "B":
+                    result = bytes[offset];
+                    return true;
+                case "u":
+                    result = BitConverter.ToChar(bytes, offset);
+                    return true;
+                case "h":
+                    result = BitConverter.ToInt16(bytes, offset);
+                    return true;
+                case "H":
+                    result = BitConverter.ToUInt16(bytes, offset);
+                    return true;
+                case "l":
+                case "i":
+                    result = BitConverter.ToInt32(bytes, offset);
+                    return true;
+                case "L":
+                case "I":
+                    result = BitConverter.ToUInt32(bytes, offset);
+                    return true;
+                case "f":
+                    result = BitConverter.ToSingle(bytes, offset);
+                    return true;
+                case "d":
+                    result = BitConverter.ToDouble(bytes, offset);
+                    return true;
+                default:
+                    result = 0;
+                    return false;
+            }
+        }
+
+        public static bool TryGetBytes(string typecode, object obj, out byte[] result) {
+            switch (typecode) {
+                case "c": result =
+                        new[] { (byte)Convert.ToChar(obj) };
+                    return true;
+                case "b":
+                    result = new[] { (byte)Convert.ToSByte(obj) };
+                    return true;
+                case "B":
+                    result = new[] { Convert.ToByte(obj) };
+                    return true;
+                case "u":
+                    result = BitConverter.GetBytes((byte)Convert.ToChar(obj));
+                    return true;
+                case "h":
+                    result = BitConverter.GetBytes(Convert.ToInt16(obj));
+                    return true;
+                case "H":
+                    result = BitConverter.GetBytes(Convert.ToUInt16(obj));
+                    return true;
+                case "l":
+                case "i":
+                    result = BitConverter.GetBytes(Convert.ToInt32(obj));
+                    return true;
+                case "L":
+                case "I":
+                    result = BitConverter.GetBytes(Convert.ToUInt32(obj));
+                    return true;
+                case "f":
+                    result = BitConverter.GetBytes(Convert.ToSingle(obj));
+                    return true;
+                case "d":
+                    result = BitConverter.GetBytes(Convert.ToDouble(obj));
+                    return true;
+                default:
+                    result = null;
+                    return false;
+            }
+        }
+    }
+}

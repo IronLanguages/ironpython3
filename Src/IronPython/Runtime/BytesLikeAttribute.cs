f4adb4f2fb8cb26336c5f2e4be7fcaf220ed06f5
Stéphane Lozier <slozier@users.noreply.github.com>
2020-03-13 22:08:23 -0400

    Add missing file

---
+++ Src/IronPython/Runtime/BytesLikeAttribute.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
+
+using System;
+
+namespace IronPython.Runtime {
+    /// <summary>
+    /// For IList<byte/> arguments: Marks that the argument is typed to accept a bytes or
+    /// bytearray object.  This attribute disallows passing a Python list object and
+    /// auto-applying our generic conversion.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public sealed class BytesLikeAttribute : Attribute { }
+}

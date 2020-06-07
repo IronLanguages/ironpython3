1f0aec0c0acbed031564efa5f47891f4fa59f528
Stéphane Lozier <slozier@users.noreply.github.com>
2020-06-07 15:02:06 -0400

    Retry test_doc multiple times

--- Tests/test_doc.py
+++ Tests/test_doc.py
@@ -204,7 +204,6 @@ class DocTest(IronPythonTestCase):
             self.assertTrue(hasattr(e, "__doc__"))
             e.__doc__
 
-    @retryOnFailure # TODO: figure out
     def test_module_doc_cp21360(self):
         temp_filename_empty = "cp21360_empty.py"
         temp_filename = "cp21360.py"

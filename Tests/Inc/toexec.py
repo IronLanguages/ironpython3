160c2a6be030ba59bddd4164c737216ba87f2a4a
slozier <slozier@users.noreply.github.com>
2020-11-24 09:17:23 -0500

    Remove test_execfile (#1049)

--- Tests/Inc/toexec.py
+++
@@ -1,7 +0,0 @@
-# Licensed to the .NET Foundation under one or more agreements.
-# The .NET Foundation licenses this file to you under the Apache 2.0 License.
-# See the LICENSE file in the project root for more information.
-
-self.assertTrue(__name__ in  ["__main__", "test_execfile"], __name__)
-
-class C: pass

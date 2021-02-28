46a19ed0ffaf230b59fc3a2a6a4eff66a28591e5
Stéphane Lozier <slozier@users.noreply.github.com>
2021-02-28 15:05:24 -0500

    Cleanup winsound

--- Src/IronPython.Modules/_posixsubprocess.cs
+++ Src/IronPython.Modules/_posixsubprocess.cs
@@ -1,17 +1,13 @@
-﻿#if FEATURE_NATIVE
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
 
-using System;
-using System.ComponentModel;
-using System.IO;
-using System.IO.Pipes;
-using System.Runtime.InteropServices;
-
-using Microsoft.Scripting.Runtime;
+#if FEATURE_NATIVE
 
 using IronPython.Runtime;
 using IronPython.Runtime.Operations;
 
-using System.Numerics;
+using Microsoft.Scripting.Runtime;
 
 [assembly: PythonModule("_posixsubprocess", typeof(IronPython.Modules.PosixSubprocess), PlatformsAttribute.PlatformFamily.Unix)]
 namespace IronPython.Modules {

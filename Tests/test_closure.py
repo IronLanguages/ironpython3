a6fbdd056478b328762229f6f39cbb0b00c1ddc2
slozier <slozier@users.noreply.github.com>
2021-08-12 18:39:17 -0400

    Properly access closure tuple (#1276)

--- Tests/test_closure.py
+++ Tests/test_closure.py
@@ -362,4 +362,13 @@ class ClosureTest(unittest.TestCase):
         res = [[tuple(str(d) for d in z) for z in y] for y in x]
         self.assertEqual(res, [[('1',)]])
 
+    def test_ipy3_gh1275(self):
+        # https://github.com/IronLanguages/ironpython3/issues/1275
+
+        x0 = x1 = x2 = x3 = x4 = x5 = x6 = x7 = x8 = x9 = x10 = x11 = x12 = x13 = x14 = x15 = x16 = x17 = x18 = x19 = x20 = x21 = x22 = x23 = x24 = x25 = x26 = x27 = x28 = x29 = x30 = x31 = x32 = x33 = x34 = x35 = x36 = x37 = x38 = x39 = x40 = x41 = x42 = x43 = x44 = x45 = x46 = x47 = x48 = x49 = x50 = x51 = x52 = x53 = x54 = x55 = x56 = x57 = x58 = x59 = x60 = x61 = x62 = x63 = x64 = x65 = x66 = x67 = x68 = x69 = x70 = x71 = x72 = x73 = x74 = x75 = x76 = x77 = x78 = x79 = x80 = x81 = x82 = x83 = x84 = x85 = x86 = x87 = x88 = x89 = x90 = x91 = x92 = x93 = x94 = x95 = x96 = x97 = x98 = x99 = x100 = x101 = x102 = x103 = x104 = x105 = x106 = x107 = x108 = x109 = x110 = x111 = x112 = x113 = x114 = x115 = x116 = x117 = x118 = x119 = x120 = x121 = x122 = x123 = x124 = x125 = x126 = x127 = x128 = 0
+        def test():
+            (x0, x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36, x37, x38, x39, x40, x41, x42, x43, x44, x45, x46, x47, x48, x49, x50, x51, x52, x53, x54, x55, x56, x57, x58, x59, x60, x61, x62, x63, x64, x65, x66, x67, x68, x69, x70, x71, x72, x73, x74, x75, x76, x77, x78, x79, x80, x81, x82, x83, x84, x85, x86, x87, x88, x89, x90, x91, x92, x93, x94, x95, x96, x97, x98, x99, x100, x101, x102, x103, x104, x105, x106, x107, x108, x109, x110, x111, x112, x113, x114, x115, x116, x117, x118, x119, x120, x121, x122, x123, x124, x125, x126, x127, x128)
+
+        # we don't care about the result as long as the definition does not throw...
+
 run_test(__name__)

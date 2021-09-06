db932e818f2a77616abc8e2f232c4da29cb7ac7f
Stéphane Lozier <slozier@users.noreply.github.com>
2021-09-06 17:12:40 -0400

    Improved command line options handling

--- Src/IronPython/Hosting/PythonOptionsParser.cs
+++ Src/IronPython/Hosting/PythonOptionsParser.cs
@@ -20,157 +20,180 @@ namespace IronPython.Hosting {
         public PythonOptionsParser() {
         }
 
-        /// <exception cref="Exception">On error.</exception>
-        protected override void ParseArgument(string/*!*/ arg) {
-            ContractUtils.RequiresNotNull(arg, nameof(arg));
+        private ReadOnlySpan<char> HandleOptions(ReadOnlySpan<char> options) {
+            switch (options[0]) {
+                case '?':
+                case 'h':
+                    ConsoleOptions.PrintUsage = true;
+                    ConsoleOptions.Exit = true;
+                    IgnoreRemainingArgs();
+                    return default;
 
-            switch (arg) {
-                case "-B": break; // dont_write_bytecode always true in IronPython
-                case "-d": break; // debug output from parser, always False in IronPython
+                case 'B': break; // dont_write_bytecode always true in IronPython
+                case 'd': break; // debug output from parser, always False in IronPython
 
-                case "-b":
+                case 'b':
                     LanguageSetup.Options["BytesWarning"] = LanguageSetup.Options.ContainsKey("BytesWarning") ? Severity.Error : Severity.Warning;
                     break;
 
-                case "-bb":
-                    LanguageSetup.Options["BytesWarning"] = Severity.Error;
-                    break;
+                case 'c': {
+                        if (options.Length > 1) {
+                            ConsoleOptions.Command = options.Slice(1).ToString();
+                            PushArgBack();
+                        } else {
+                            ConsoleOptions.Command = PeekNextArg();
+                        }
+                        string[] arguments = PopRemainingArgs();
+                        arguments[0] = "-c";
+                        LanguageSetup.Options["Arguments"] = arguments;
+                        return default;
+                    }
 
-                case "-c":
-                    ConsoleOptions.Command = PeekNextArg();
-                    string[] arguments = PopRemainingArgs();
-                    arguments[0] = arg;
-                    LanguageSetup.Options["Arguments"] = arguments;
-                    break;
-
-                case "-i":
+                case 'i':
                     ConsoleOptions.Introspection = true;
                     LanguageSetup.Options["Inspect"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-m":
-                    ConsoleOptions.ModuleToRun = PeekNextArg();
-                    LanguageSetup.Options["Arguments"] = PopRemainingArgs();
-                    break;
+                case 'm': {
+                        if (options.Length > 1) {
+                            ConsoleOptions.ModuleToRun = options.Slice(1).ToString();
+                            PushArgBack();
+                        } else {
+                            ConsoleOptions.ModuleToRun = PeekNextArg();
+                        }
+                        string[] arguments = PopRemainingArgs();
+                        arguments[0] = ConsoleOptions.ModuleToRun;
+                        LanguageSetup.Options["Arguments"] = arguments;
+                        return default;
+                    }
 
-                case "-x":
+                case 'x':
                     ConsoleOptions.SkipFirstSourceLine = true;
                     break;
 
                 // TODO: unbuffered stdout?
-                case "-u": break;
+                case 'u': break;
 
                 // TODO: create a trace listener?
-                case "-v":
+                case 'v':
                     LanguageSetup.Options["Verbose"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-I": // both -E and -s
+                case 'I': // both -E and -s
                     ConsoleOptions.IgnoreEnvironmentVariables = true;
                     LanguageSetup.Options["IgnoreEnvironment"] = ScriptingRuntimeHelpers.True;
                     LanguageSetup.Options["NoUserSite"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-S":
+                case 'S':
                     ConsoleOptions.SkipImportSite = true;
                     LanguageSetup.Options["NoSite"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-s":
+                case 's':
                     LanguageSetup.Options["NoUserSite"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-E":
+                case 'E':
                     ConsoleOptions.IgnoreEnvironmentVariables = true;
                     LanguageSetup.Options["IgnoreEnvironment"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-t":
-                    //ignore for backwards compatibility
-                    break;
+                case 't': break; // ignore for backwards compatibility
 
-                case "-tt":
-                    //ignore for backwards compatibility
-                    break;
-
-                case "-O":
+                case 'O':
+                    if (LanguageSetup.Options.ContainsKey("Optimize")) LanguageSetup.Options["StripDocStrings"] = ScriptingRuntimeHelpers.True;
                     LanguageSetup.Options["Optimize"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-OO":
-                    LanguageSetup.Options["Optimize"] = ScriptingRuntimeHelpers.True;
-                    LanguageSetup.Options["StripDocStrings"] = ScriptingRuntimeHelpers.True;
-                    break;
-
-                case "-V":
+                case 'V':
                     ConsoleOptions.PrintVersion = true;
                     ConsoleOptions.Exit = true;
                     IgnoreRemainingArgs();
+                    return default;
+
+                case 'W':
+                    _warningFilters ??= new List<string>();
+                    _warningFilters.Add(options.Length > 1 ? options.Slice(1).ToString() : PopNextArg());
                     break;
 
-                case "-W":
-                    if (_warningFilters == null) {
-                        _warningFilters = new List<string>();
-                    }
-
-                    _warningFilters.Add(PopNextArg());
-                    break;
-
-                case "-q":
+                case 'q':
                     LanguageSetup.Options["Quiet"] = ScriptingRuntimeHelpers.True;
                     break;
 
-                case "-":
-                    PushArgBack();
-                    LanguageSetup.Options["Arguments"] = PopRemainingArgs();
-                    break;
-
-                // old implementation specific options for compat
-                case "-X:NoFrames":
-                case "-X:Frames":
-                case "-X:FullFrames":
-                case "-X:Tracing":
-                case "-X:EnableProfiler":
-                case "-X:LightweightScopes":
-                case "-X:MTA":
-                case "-X:Debug":
-                case "-X:BasicConsole":
-#if DEBUG
-                case "-X:NoImportLib":
-#endif
-                    HandleImplementationSpecificOption(arg.Substring(3), null);
-                    break;
-
-                case "-X:GCStress":
-                case "-X:MaxRecursion":
-                case "-X:NoDebug":
-                    HandleImplementationSpecificOption(arg.Substring(3), PopNextArg());
-                    break;
+                case 'X':
+                    var split = (options.Length > 1 ? options.Slice(1).ToString() : PopNextArg()).Split(new[] { '=' }, 2);
+                    HandleImplementationSpecificOption(split[0], split.Length > 1 ? split[1] : null);
+                    return default;
 
                 default:
-                    if (arg.StartsWith("-W", StringComparison.Ordinal)) {
-                        if (_warningFilters == null) {
-                            _warningFilters = new List<string>();
-                        }
-
-                        _warningFilters.Add(arg.Substring(2));
-                        break;
-                    }
-
-                    if (arg.StartsWith("-m", StringComparison.Ordinal)) {
-                        ConsoleOptions.ModuleToRun = arg.Substring(2);
-                        LanguageSetup.Options["Arguments"] = PopRemainingArgs();
-                        break;
-                    }
-
-                    base.ParseArgument(arg);
-
-                    if (ConsoleOptions.FileName != null) {
-                        PushArgBack();
-                        LanguageSetup.Options["Arguments"] = PopRemainingArgs();
-                    }
-                    break;
+                    throw new InvalidOptionException($"Unknown option: {options[0]}");
             }
+            return options.Slice(1);
+        }
+
+        /// <exception cref="Exception">On error.</exception>
+        protected override void ParseArgument(string/*!*/ arg) {
+            ContractUtils.RequiresNotNull(arg, nameof(arg));
+
+            if (arg == "-") {
+                PushArgBack();
+                LanguageSetup.Options["Arguments"] = PopRemainingArgs();
+                return;
+            }
+
+            if (arg == "/?" || arg == "--help") {
+                HandleOptions("h".AsSpan());
+                return;
+            }
+
+            if (arg.StartsWith("-", StringComparison.Ordinal)) {
+                if (arg.StartsWith("-X:", StringComparison.Ordinal)) {
+                    // old implementation specific options for compat
+                    switch (arg) {
+                        case "-X:NoFrames":
+                        case "-X:Frames":
+                        case "-X:FullFrames":
+                        case "-X:Tracing":
+                        case "-X:EnableProfiler":
+                        case "-X:LightweightScopes":
+                        case "-X:MTA":
+                        case "-X:Debug":
+                        case "-X:BasicConsole":
+#if DEBUG
+                        case "-X:NoImportLib":
+#endif
+                            HandleImplementationSpecificOption(arg.Substring(3), null);
+                            break;
+
+                        case "-X:GCStress":
+                        case "-X:MaxRecursion":
+                        case "-X:NoDebug":
+                            HandleImplementationSpecificOption(arg.Substring(3), PopNextArg());
+                            break;
+
+                        default:
+                            base.ParseArgument(arg);
+                            if (ConsoleOptions.FileName != null) {
+                                // Note that CPython ignores unknown implementation-specific options
+                                // but we'll throw when -X:Unknown is used to avoid potential mixing
+                                // of old/new style errors (e.g. -X:CompilationThreshold=1).
+                                throw new InvalidOptionException($"Unknown option: {arg}");
+                            }
+                            return;
+                    }
+                } else {
+                    var options = arg.AsSpan(1);
+                    while (options.Length > 0) {
+                        options = HandleOptions(options);
+                    }
+                    return;
+                }
+            }
+
+            ConsoleOptions.FileName = arg.Trim();
+            PushArgBack();
+            LanguageSetup.Options["Arguments"] = PopRemainingArgs();
         }
 
         protected override void HandleImplementationSpecificOption(string arg, string val) {
@@ -212,7 +235,7 @@ namespace IronPython.Hosting {
                     // we need about 6 frames for starting up, so 10 is a nice round number.
                     int limit;
                     if (!int.TryParse(val, out limit) || limit < 10) {
-                        throw new InvalidOptionException(string.Format("The argument for the -X {0} option must be an integer >= 10.", arg));
+                        throw new InvalidOptionException($"The argument for the -X {arg} option must be an integer >= 10.");
                     }
                     LanguageSetup.Options["RecursionLimit"] = limit;
                     break;
@@ -239,7 +262,7 @@ namespace IronPython.Hosting {
                     try {
                         LanguageSetup.Options["NoDebug"] = new Regex(val);
                     } catch {
-                        throw InvalidOptionValue("NoDebug", val);
+                        throw new InvalidOptionException($"The argument for the -X {arg} option must be a valid regex pattern.");
                     }
                     break;
 
@@ -254,7 +277,11 @@ namespace IronPython.Hosting {
 #endif
 
                 default:
-                    base.HandleImplementationSpecificOption(arg, val);
+                    try {
+                        base.HandleImplementationSpecificOption(arg, val);
+                    } catch (InvalidOptionException e) {
+                        throw new InvalidOptionException(e.Message.Replace("-X:", "-X "));
+                    }
                     break;
             }
         }
@@ -265,33 +292,45 @@ namespace IronPython.Hosting {
             }
         }
 
-        protected override bool UseImplementationSpecificOptions { get; } = true;
-
         public override void GetHelp(out string commandLine, out string[,] options, out string[,] environmentVariables, out string comments) {
             string[,] standardOptions;
-            base.GetHelp(out commandLine, out standardOptions, out environmentVariables, out comments);
-#if !IRONPYTHON_WINDOW
-            commandLine = "Usage: ipy [options] [file.py|- [arguments]]";
-#else
-            commandLine = "Usage: ipyw [options] [file.py|- [arguments]]";
-#endif
+            base.GetHelp(out _, out standardOptions, out environmentVariables, out comments);
+
+            commandLine = "[option] ... [-c cmd | -m mod | file | -] [arg] ...";
+
+            // only keep the implementation-specific options from the DLR
+            List<KeyValuePair<string, string>> standardOptionsList = new();
+            for (var i = 0; i < standardOptions.GetLength(0); i++) {
+                if (standardOptions[i, 0].StartsWith("-X:", StringComparison.Ordinal)) {
+                    standardOptionsList.Add(new KeyValuePair<string, string>(standardOptions[i, 0].Replace(' ', '=').Replace(':', ' '), standardOptions[i, 1]));
+                }
+            }
+            standardOptions = new string[standardOptionsList.Count, 2];
+            for (var i = 0; i < standardOptionsList.Count; i++) {
+                standardOptions[i, 0] = standardOptionsList[i].Key;
+                standardOptions[i, 1] = standardOptionsList[i].Value;
+            }
 
             string[,] pythonOptions = new string[,] {
+                { "-b",                     "issue warnings about str(bytes_instance), str(bytearray_instance) and comparing bytes/bytearray with str. (-bb: issue errors)"},
+                { "-c cmd",                 "Program passed in as string (terminates option list)" },
+                { "-E",                     "Ignore environment variables" },
+                { "-h",                     "Display usage" },
+                { "-i",                     "Inspect interactively after running script" },
+                { "-I",                     "isolate IronPython from the user's environment (implies -E and -s)" },
+                { "-m mod",                 "run library module as a script"},
+                { "-O",                     "generate optimized code" },
+                { "-OO",                    "remove doc strings and apply -O optimizations" },
+                { "-q",                     "don't print version and copyright messages on interactive startup" },
+                { "-s",                     "Don't add user site directory to sys.path" },
+                { "-S",                     "Don't imply 'import site' on initialization" },
+                { "-u",                     "Unbuffered stdout & stderr" },
 #if !IRONPYTHON_WINDOW
                 { "-v",                     "Verbose (trace import statements) (also PYTHONVERBOSE=x)" },
 #endif
-                { "-b",                     "issue warnings about str(bytes_instance), str(bytearray_instance) and comparing bytes/bytearray with str. (-bb: issue errors)"},
-                { "-m mod",                 "run library module as a script"},
-                { "-x",                     "Skip first line of the source" },
-                { "-u",                     "Unbuffered stdout & stderr" },
-                { "-O",                     "generate optimized code" },
-                { "-OO",                    "remove doc strings and apply -O optimizations" },
-                { "-E",                     "Ignore environment variables" },
-                { "-S",                     "Don't imply 'import site' on initialization" },
-                { "-s",                     "Don't add user site directory to sys.path" },
-                { "-I",                     "isolate IronPython from the user's environment (implies -E and -s)" },
+                { "-V",                     "Print the version number and exit" },
                 { "-W arg",                 "Warning control (arg is action:message:category:module:lineno) also IRONPYTHONWARNINGS=arg" },
-                { "-q",                     "don't print version and copyright messages on interactive startup" },
+                { "-x",                     "Skip first line of the source" },
 
                 { "-X NoFrames",            "Disable sys._getframe support, can improve execution speed" },
                 { "-X Frames",              "Enable basic sys._getframe support" },
@@ -312,17 +351,22 @@ namespace IronPython.Hosting {
 
             // Ensure the combined options come out sorted
             string[,] allOptions = ArrayUtils.Concatenate(pythonOptions, standardOptions);
+            var opts = new List<KeyValuePair<string, string>>();
+            for (var i = 0; i < allOptions.GetLength(0); i++) {
+                opts.Add(new KeyValuePair<string, string>(allOptions[i, 0], allOptions[i, 1]));
+            }
             List<string> optName = new List<string>();
             List<int> indiciesList = new List<int>();
-            for (int i = 0; i < allOptions.Length / 2; i++) {
+            for (int i = 0; i < allOptions.GetLength(0); i++) {
                 optName.Add(allOptions[i, 0]);
                 indiciesList.Add(i);
             }
-            
-            int[] indicies = indiciesList.ToArray();
-            Array.Sort(optName.ToArray(), indicies, StringComparer.OrdinalIgnoreCase);
 
-            options = new string[allOptions.Length / 2, 2];
+            var keys = optName.ToArray();
+            var indicies = indiciesList.ToArray();
+            Array.Sort(keys, indicies, StringComparer.OrdinalIgnoreCase);
+
+            options = new string[allOptions.GetLength(0), 2];
             for (int i = 0; i < indicies.Length; i++) {
                 options[i, 0] = allOptions[indicies[i], 0];
                 options[i, 1] = allOptions[indicies[i], 1];
@@ -333,7 +377,6 @@ namespace IronPython.Hosting {
                 { "IRONPYTHONPATH",        "Path to search for module" },
                 { "IRONPYTHONSTARTUP",     "Startup module" }
             };
-
         }
     }
 }

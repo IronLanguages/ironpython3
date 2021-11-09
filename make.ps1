12111c015883f12524a5ee2fc0dd532ba4e5de23
Stéphane Lozier <slozier@users.noreply.github.com>
2021-11-09 12:56:49 -0500

    Check prerelease in vswhere

--- make.ps1
+++ make.ps1
@@ -22,7 +22,8 @@ function EnsureMSBuild() {
     $_VSINSTPATH = ''
 
     if([System.IO.File]::Exists($_VSWHERE)) {
-        $_VSINSTPATH = & "$_VSWHERE" -latest -requires Microsoft.Component.MSBuild -property installationPath
+        $_VSINSTPATH = & "$_VSWHERE" -latest -prerelease -requires Microsoft.Component.MSBuild -property installationPath
+        Write-Host $_VSINSTPATH
     } else {
         Write-Error "Visual Studio 2019 16.8 or later is required"
         Exit 1

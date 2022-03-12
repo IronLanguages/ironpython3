59d7446c2305e76e01470b186fa0cf2c710a6955
Stéphane Lozier <slozier@users.noreply.github.com>
2022-03-12 16:19:56 -0500

    Add .NET Foundation headers

--- Src/IronPython/Compiler/Ast/JoinedStringExpression.cs
+++ Src/IronPython/Compiler/Ast/JoinedStringExpression.cs
@@ -1,4 +1,8 @@
-﻿#nullable enable
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the Apache 2.0 License.
+// See the LICENSE file in the project root for more information.
+
+#nullable enable
 
 using System;
 using System.Collections.Generic;

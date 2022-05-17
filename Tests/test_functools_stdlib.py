50180f96c16a4129f399b42d0a2aa44830b436e3
Stéphane Lozier <slozier@users.noreply.github.com>
2022-05-16 21:37:01 -0400

    Re-enable test_functools_stdlib

--- Tests/test_functools_stdlib.py
+++ Tests/test_functools_stdlib.py
@@ -33,12 +33,12 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestLRUC('test_copy'))
         suite.addTest(test.test_functools.TestLRUC('test_deepcopy'))
         suite.addTest(test.test_functools.TestLRUC('test_early_detection_of_bad_call'))
-        suite.addTest(test.test_functools.TestLRUC('test_kwargs_order'))
+        #suite.addTest(test.test_functools.TestLRUC('test_kwargs_order')) # intermittent failures - https://github.com/IronLanguages/ironpython3/issues/1460
         suite.addTest(test.test_functools.TestLRUC('test_lru'))
         suite.addTest(test.test_functools.TestLRUC('test_lru_cache_decoration'))
-        suite.addTest(test.test_functools.TestLRUC('test_lru_cache_threaded'))
-        suite.addTest(test.test_functools.TestLRUC('test_lru_cache_threaded2'))
-        suite.addTest(test.test_functools.TestLRUC('test_lru_cache_threaded3'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestLRUC('test_lru_cache_threaded'))) # unittest.expectedFailure(
+        #suite.addTest(test.test_functools.TestLRUC('test_lru_cache_threaded2')) # intermittent failures
+        #suite.addTest(test.test_functools.TestLRUC('test_lru_cache_threaded3')) # intermittent failures
         suite.addTest(test.test_functools.TestLRUC('test_lru_method'))
         suite.addTest(test.test_functools.TestLRUC('test_lru_reentrancy_with_len'))
         suite.addTest(test.test_functools.TestLRUC('test_lru_type_error'))
@@ -49,14 +49,14 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestLRUC('test_lru_with_maxsize_none'))
         suite.addTest(test.test_functools.TestLRUC('test_lru_with_types'))
         suite.addTest(test.test_functools.TestLRUC('test_need_for_rlock'))
-        suite.addTest(test.test_functools.TestLRUC('test_pickle'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestLRUC('test_pickle'))) # pickle.PicklingError: Can't pickle
         suite.addTest(test.test_functools.TestLRUPy('test_copy'))
         suite.addTest(test.test_functools.TestLRUPy('test_deepcopy'))
         suite.addTest(test.test_functools.TestLRUPy('test_early_detection_of_bad_call'))
-        suite.addTest(test.test_functools.TestLRUPy('test_kwargs_order'))
+        #suite.addTest(test.test_functools.TestLRUPy('test_kwargs_order')) # intermittent failures - https://github.com/IronLanguages/ironpython3/issues/1460
         suite.addTest(test.test_functools.TestLRUPy('test_lru'))
         suite.addTest(test.test_functools.TestLRUPy('test_lru_cache_decoration'))
-        suite.addTest(test.test_functools.TestLRUPy('test_lru_cache_threaded'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestLRUPy('test_lru_cache_threaded'))) # AttributeError: 'module' object has no attribute 'getswitchinterval'
         suite.addTest(test.test_functools.TestLRUPy('test_lru_cache_threaded2'))
         suite.addTest(test.test_functools.TestLRUPy('test_lru_cache_threaded3'))
         suite.addTest(test.test_functools.TestLRUPy('test_lru_method'))
@@ -69,33 +69,33 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestLRUPy('test_lru_with_maxsize_none'))
         suite.addTest(test.test_functools.TestLRUPy('test_lru_with_types'))
         suite.addTest(test.test_functools.TestLRUPy('test_need_for_rlock'))
-        suite.addTest(test.test_functools.TestLRUPy('test_pickle'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestLRUPy('test_pickle'))) # pickle.PicklingError: Can't pickle
         suite.addTest(test.test_functools.TestPartialC('test_arg_combinations'))
         suite.addTest(test.test_functools.TestPartialC('test_argument_checking'))
         suite.addTest(test.test_functools.TestPartialC('test_attributes'))
         suite.addTest(test.test_functools.TestPartialC('test_attributes_unwritable'))
         suite.addTest(test.test_functools.TestPartialC('test_basic_examples'))
-        suite.addTest(test.test_functools.TestPartialC('test_copy'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_copy'))) # AssertionError: (['asdf'],) is not (['asdf'],)
         suite.addTest(test.test_functools.TestPartialC('test_deepcopy'))
         suite.addTest(test.test_functools.TestPartialC('test_error_propagation'))
-        suite.addTest(test.test_functools.TestPartialC('test_keystr_replaces_value'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_keystr_replaces_value'))) # AssertionError: 'astr' not found in '<CPartialSubclass object at 0x00000000000000A8>'
         suite.addTest(test.test_functools.TestPartialC('test_keyword'))
         suite.addTest(test.test_functools.TestPartialC('test_kw_combinations'))
         suite.addTest(test.test_functools.TestPartialC('test_kwargs_copy'))
-        suite.addTest(test.test_functools.TestPartialC('test_manually_adding_non_string_keyword'))
-        suite.addTest(test.test_functools.TestPartialC('test_nested_optimization'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_manually_adding_non_string_keyword'))) # AssertionError: '1234' not found in '<CPartialSubclass object at 0x00000000000000AA>'
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_nested_optimization'))) # AssertionError: Tuples differ: (<partial object at 0x0000000000000066>, (), {'bar': True}, {}) != (<function signature at 0x0000000000000051>, ('asdf',), {'bar': True}, {})
         suite.addTest(test.test_functools.TestPartialC('test_nested_partial_with_attribute'))
         suite.addTest(test.test_functools.TestPartialC('test_no_side_effects'))
         suite.addTest(test.test_functools.TestPartialC('test_pickle'))
         suite.addTest(test.test_functools.TestPartialC('test_positional'))
         suite.addTest(test.test_functools.TestPartialC('test_protection_of_callers_dict_argument'))
-        suite.addTest(test.test_functools.TestPartialC('test_recursive_pickle'))
-        suite.addTest(test.test_functools.TestPartialC('test_recursive_repr'))
+        #suite.addTest(test.test_functools.TestPartialC('test_recursive_pickle')) # StackOverflowException
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_recursive_repr'))) # AssertionError: '<CPartialSubclass object at 0x00000000000000C0>' != 'CPartialSubclass(...)'
         suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_repr'))) # AssertionError
-        suite.addTest(test.test_functools.TestPartialC('test_setstate'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_setstate'))) # AssertionError: Tuples differ: (<function capture at 0x000000000000008D>, (1,), {'a': 10}, {'attr': []}) != (<function capture at 0x000000000000008D>, (1,), {'a': 10}, {})
         suite.addTest(test.test_functools.TestPartialC('test_setstate_errors'))
-        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_setstate_refcount'))) # AttributeError: 'partial' object has no attribute '__setstate__'
-        suite.addTest(test.test_functools.TestPartialC('test_setstate_subclasses'))
+        suite.addTest(test.test_functools.TestPartialC('test_setstate_refcount'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialC('test_setstate_subclasses'))) # AssertionError: <class 'test.test_functools.MyDict'> is not <class 'dict'>
         suite.addTest(test.test_functools.TestPartialC('test_weakref'))
         suite.addTest(test.test_functools.TestPartialC('test_with_bound_and_unbound_methods'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_arg_combinations'))
@@ -103,26 +103,26 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestPartialCSubclass('test_attributes'))
         suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_attributes_unwritable'))) # AssertionError: AttributeError not raised by setattr
         suite.addTest(test.test_functools.TestPartialCSubclass('test_basic_examples'))
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_copy'))
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_deepcopy'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_copy'))) # AttributeError: 'partial' object has no attribute 'attr'
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_deepcopy'))) # AttributeError: 'partial' object has no attribute 'attr'
         suite.addTest(test.test_functools.TestPartialCSubclass('test_error_propagation'))
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_keystr_replaces_value'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_keystr_replaces_value'))) # AssertionError: 'astr' not found in '<CPartialSubclass object at 0x00000000000000A8>'
         suite.addTest(test.test_functools.TestPartialCSubclass('test_keyword'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_kw_combinations'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_kwargs_copy'))
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_manually_adding_non_string_keyword'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_manually_adding_non_string_keyword'))) # AssertionError: '1234' not found in '<CPartialSubclass object at 0x00000000000000AA>'
         suite.addTest(test.test_functools.TestPartialCSubclass('test_nested_partial_with_attribute'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_no_side_effects'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_pickle'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_positional'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_protection_of_callers_dict_argument'))
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_recursive_pickle'))
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_recursive_repr'))
+        #suite.addTest(test.test_functools.TestPartialCSubclass('test_recursive_pickle')) # StackOverflowException
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_recursive_repr'))) # AssertionError: '<CPartialSubclass object at 0x00000000000000C0>' != 'CPartialSubclass(...)'
         suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_repr'))) # AssertionError
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_setstate'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_setstate'))) # AssertionError: Tuples differ: (<function capture at 0x000000000000008D>, (1,), {'a': 10}, {'attr': []}) != (<function capture at 0x000000000000008D>, (1,), {'a': 10}, {})
         suite.addTest(test.test_functools.TestPartialCSubclass('test_setstate_errors'))
-        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_setstate_refcount'))) # AttributeError: 'PartialSubclass' object has no attribute '__setstate__'
-        suite.addTest(test.test_functools.TestPartialCSubclass('test_setstate_subclasses'))
+        suite.addTest(test.test_functools.TestPartialCSubclass('test_setstate_refcount'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestPartialCSubclass('test_setstate_subclasses'))) # AssertionError: <class 'test.test_functools.MyDict'> is not <class 'dict'>
         suite.addTest(test.test_functools.TestPartialCSubclass('test_weakref'))
         suite.addTest(test.test_functools.TestPartialCSubclass('test_with_bound_and_unbound_methods'))
         suite.addTest(test.test_functools.TestPartialMethod('test_abstract'))
@@ -151,7 +151,7 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestPartialPy('test_pickle'))
         suite.addTest(test.test_functools.TestPartialPy('test_positional'))
         suite.addTest(test.test_functools.TestPartialPy('test_protection_of_callers_dict_argument'))
-        suite.addTest(test.test_functools.TestPartialPy('test_recursive_pickle'))
+        #suite.addTest(test.test_functools.TestPartialPy('test_recursive_pickle')) #  # StackOverflowException
         suite.addTest(test.test_functools.TestPartialPy('test_recursive_repr'))
         suite.addTest(test.test_functools.TestPartialPy('test_repr'))
         suite.addTest(test.test_functools.TestPartialPy('test_setstate'))
@@ -177,7 +177,7 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestPartialPySubclass('test_pickle'))
         suite.addTest(test.test_functools.TestPartialPySubclass('test_positional'))
         suite.addTest(test.test_functools.TestPartialPySubclass('test_protection_of_callers_dict_argument'))
-        suite.addTest(test.test_functools.TestPartialPySubclass('test_recursive_pickle'))
+        #suite.addTest(test.test_functools.TestPartialPySubclass('test_recursive_pickle')) # StackOverflowException
         suite.addTest(test.test_functools.TestPartialPySubclass('test_recursive_repr'))
         suite.addTest(test.test_functools.TestPartialPySubclass('test_repr'))
         suite.addTest(test.test_functools.TestPartialPySubclass('test_setstate'))
@@ -201,7 +201,7 @@ def load_tests(loader, standard_tests, pattern):
         suite.addTest(test.test_functools.TestSingleDispatch('test_simple_overloads'))
         suite.addTest(test.test_functools.TestSingleDispatch('test_wrapping_attributes'))
         suite.addTest(test.test_functools.TestTotalOrdering('test_no_operations_defined'))
-        suite.addTest(test.test_functools.TestTotalOrdering('test_pickle'))
+        suite.addTest(unittest.expectedFailure(test.test_functools.TestTotalOrdering('test_pickle'))) # pickle.PicklingError: Can't pickle
         suite.addTest(test.test_functools.TestTotalOrdering('test_total_ordering_ge'))
         suite.addTest(test.test_functools.TestTotalOrdering('test_total_ordering_gt'))
         suite.addTest(test.test_functools.TestTotalOrdering('test_total_ordering_le'))

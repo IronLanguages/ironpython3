2f10037ab4691978d4475f5feb4c0a18c87691f4
Pavel Koneski <pavel.koneski@gmail.com>
2022-07-13 20:53:20 -0700

    Support module errno on Linux and macOS (#1502)
    * Generate platform-specific errno codes
    
    * Generate platform-specific errno symbols
    
    * Add test_errno
    
    * Support error symbol aliases
    
    * Support error symbol aliases on Linux
    
    * Support error symbol aliases on Darwin
    
    * Include Posix aliases in Windows aliases
    
    * Stable errno order
    
    * Let _socket use OS-native error codes
    
    * Make tests passing on all platforms
    
    * More Mono codes remapped
    
    * Complete mapping of Mono codes

--- Src/IronPython/Runtime/PythonHiddenAttribute.cs
+++ Src/IronPython/Runtime/PythonHiddenAttribute.cs
@@ -9,6 +9,7 @@ namespace IronPython.Runtime {
     /// <summary>
     /// Marks a member as being hidden from Python code.
     /// </summary>
+    /// <inheritdoc />
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Class, AllowMultiple = false, Inherited = false)]
     public sealed class PythonHiddenAttribute : PlatformsAttribute {
         public PythonHiddenAttribute(params PlatformID[] hiddenPlatforms) {
@@ -17,6 +18,14 @@ namespace IronPython.Runtime {
 
         public PythonHiddenAttribute(PlatformsAttribute.PlatformFamily hiddenPlatformFamily) {
             SetValidPlatforms(hiddenPlatformFamily);
+        }
+
+        public PythonHiddenAttribute(PlatformsAttribute.PlatformFamily hiddenPlatformFamily, params PlatformID[] hiddenPlatforms)
+            : this(hiddenPlatformFamily) {
+            var allHiddenPlatforms = new PlatformID[ValidPlatforms.Length + hiddenPlatforms.Length];
+            Array.Copy(ValidPlatforms, allHiddenPlatforms, ValidPlatforms.Length);
+            Array.Copy(hiddenPlatforms, 0, allHiddenPlatforms, ValidPlatforms.Length, hiddenPlatforms.Length);
+            ValidPlatforms = allHiddenPlatforms;
         }
 
         public static bool IsHidden(MemberInfo m, bool inherit = false) {

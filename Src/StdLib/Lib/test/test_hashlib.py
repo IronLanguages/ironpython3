8a4cd87c5533acb4ffdb8ef72e8889d4b7a7874c
Stéphane Lozier <slozier@users.noreply.github.com>
2022-07-18 16:47:11 -0400

    Re-enable test_hashlib

--- Src/StdLib/Lib/test/test_hashlib.py
+++ Src/StdLib/Lib/test/test_hashlib.py
@@ -75,9 +75,11 @@ class HashLibTestCase(unittest.TestCase):
     supported_hash_names = ( 'md5', 'MD5', 'sha1', 'SHA1',
                              'sha224', 'SHA224', 'sha256', 'SHA256',
                              'sha384', 'SHA384', 'sha512', 'SHA512',
-                             'blake2b', 'blake2s',
-                             'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
-                             'shake_128', 'shake_256')
+                            )
+# https://github.com/IronLanguages/ironpython3/issues/1429
+#                             'blake2b', 'blake2s',
+#                             'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512',
+#                             'shake_128', 'shake_256')
 
     shakes = {'shake_128', 'shake_256'}
 

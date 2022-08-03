d0c30ffd8f302da146c6f1258cab2ed45d00a414
Stéphane Lozier <slozier@users.noreply.github.com>
2022-08-02 21:41:42 -0400

    Re-enable test_uuid

--- Src/IronPython/Runtime/Operations/StringOps.cs
+++ Src/IronPython/Runtime/Operations/StringOps.cs
@@ -1991,17 +1991,28 @@ namespace IronPython.Runtime.Operations {
 
         internal static partial class CodecsInfo {
             internal static readonly Encoding MbcsEncoding;
+            internal static readonly Encoding OemEncoding;
             internal static readonly Encoding RawUnicodeEscapeEncoding = new UnicodeEscapeEncoding(raw: true);
             internal static readonly Encoding UnicodeEscapeEncoding = new UnicodeEscapeEncoding(raw: false);
             internal static readonly IDictionary<string, Lazy<Encoding?>> Codecs;
+
+            [DllImport(Interop.Libraries.Kernel32, ExactSpelling = true, CharSet = CharSet.Auto)]
+            private static extern int GetOEMCP();
 
             static CodecsInfo() {
 #if NETCOREAPP || NETSTANDARD
                 // This ensures that Encoding.GetEncoding(0) will return the default Windows ANSI code page
                 Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 #endif
-                // Use Encoding.GetEncoding(0) instead of Encoding.Default (which returns UTF-8 with .NET Core)
-                MbcsEncoding = Encoding.GetEncoding(0);
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                    // Use Encoding.GetEncoding(0) instead of Encoding.Default (which returns UTF-8 with .NET Core)
+                    MbcsEncoding = Encoding.GetEncoding(0);
+                    OemEncoding = Encoding.GetEncoding(GetOEMCP());
+                } else {
+                    MbcsEncoding = null!;
+                    OemEncoding = null!;
+                }
                 Codecs = MakeCodecsDict();
             }
 
@@ -2030,6 +2041,7 @@ namespace IronPython.Runtime.Operations {
 
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
                     d["mbcs"] = makeEncodingProxy(() => MbcsEncoding);
+                    d["oem"] = makeEncodingProxy(() => OemEncoding);
                 }
 
                 // TODO: revisit the exceptions to rules below once _codecs_cn, _codecs_hk, _codecs_jp, and _codecs_kr are implemented

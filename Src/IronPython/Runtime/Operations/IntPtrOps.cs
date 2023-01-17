71cdd4ec4d603997758712f479b029ed9226b7a0
slozier <slozier@users.noreply.github.com>
2023-01-17 09:16:56 -0500

    Add IntPtr/UIntPtr constructors (#1653)

--- Src/IronPython/Runtime/Operations/IntPtrOps.cs
+++ Src/IronPython/Runtime/Operations/IntPtrOps.cs
@@ -5,10 +5,57 @@
 #nullable enable
 
 using System;
+using System.Numerics;
 using System.Runtime.CompilerServices;
+
+using IronPython.Runtime.Types;
+
+using Microsoft.Scripting.Runtime;
 
 namespace IronPython.Runtime.Operations {
     public static class IntPtrOps {
+
+        #region Constructors
+
+        [StaticExtensionMethod]
+        public static object __new__(PythonType cls)
+            => __new__(cls, default(IntPtr));
+
+        [StaticExtensionMethod]
+        public static object __new__(PythonType cls, object value) {
+            if (cls != DynamicHelpers.GetPythonTypeFromType(typeof(IntPtr))) {
+                throw PythonOps.TypeError("IntPtr.__new__: first argument must be IntPtr type.");
+            }
+            switch (value) {
+                case IntPtr:
+                    return value;
+                case IConvertible valueConvertible:
+                    switch (valueConvertible.GetTypeCode()) {
+                        case TypeCode.Byte: return checked((IntPtr)(byte)value);
+                        case TypeCode.SByte: return checked((IntPtr)(sbyte)value);
+                        case TypeCode.Int16: return checked((IntPtr)(short)value);
+                        case TypeCode.UInt16: return checked((IntPtr)(ushort)value);
+                        case TypeCode.Int32: return checked((IntPtr)(int)value);
+                        case TypeCode.UInt32: return checked((IntPtr)(uint)value);
+                        case TypeCode.Int64: return checked((IntPtr)(long)value);
+                        case TypeCode.UInt64: return checked((IntPtr)(ulong)value);
+                    }
+                    break;
+                case BigInteger bi:
+                    if (IntPtr.Size == 4) {
+                        return checked((IntPtr)(int)bi);
+                    }
+                    return checked((IntPtr)(long)bi);
+                case Extensible<BigInteger> ebi:
+                    if (IntPtr.Size == 4) {
+                        return checked((IntPtr)(int)ebi.Value);
+                    }
+                    return checked((IntPtr)(long)ebi.Value);
+            }
+            throw PythonOps.TypeError("can't convert {0} to IntPtr", PythonOps.GetPythonTypeName(value));
+        }
+
+        #endregion
 
         #region Binary Operations - Comparisons
 

d8b570a56a1bfe0c6942e6857719fb1cb3df136e
Pavel Koneski <pavel.koneski@gmail.com>
2023-07-13 17:14:47 -0700

    Use OS-specific values for file open flags (#1709)
    * Use platform-specific values for O_flags
    
    * Rename generate_errno.py to generate_os_codes.py
    
    * Remove optional flags

--- Src/Scripts/generate_os_codes.py
+++ Src/Scripts/generate_os_codes.py
@@ -3,9 +3,10 @@
 # See the LICENSE file in the project root for more information.
 
 from generate import generate
+from collections import OrderedDict
 
-num_systems = 3
-linux_idx, darwin_idx, windows_idx = range(num_systems)
+systems = ['linux', 'macos', 'windows']
+linux_idx, darwin_idx, windows_idx = range(len(systems))
 
 # python3 -c 'import errno;print(dict(sorted(errno.errorcode.items())))'
 errorcode_linux   = {1: 'EPERM', 2: 'ENOENT', 3: 'ESRCH', 4: 'EINTR', 5: 'EIO', 6: 'ENXIO', 7: 'E2BIG', 8: 'ENOEXEC', 9: 'EBADF', 10: 'ECHILD', 11: 'EAGAIN', 12: 'ENOMEM', 13: 'EACCES', 14: 'EFAULT', 15: 'ENOTBLK', 16: 'EBUSY', 17: 'EEXIST', 18: 'EXDEV', 19: 'ENODEV', 20: 'ENOTDIR', 21: 'EISDIR', 22: 'EINVAL', 23: 'ENFILE', 24: 'EMFILE', 25: 'ENOTTY', 26: 'ETXTBSY', 27: 'EFBIG', 28: 'ENOSPC', 29: 'ESPIPE', 30: 'EROFS', 31: 'EMLINK', 32: 'EPIPE', 33: 'EDOM', 34: 'ERANGE', 35: 'EDEADLOCK', 36: 'ENAMETOOLONG', 37: 'ENOLCK', 38: 'ENOSYS', 39: 'ENOTEMPTY', 40: 'ELOOP', 42: 'ENOMSG', 43: 'EIDRM', 44: 'ECHRNG', 45: 'EL2NSYNC', 46: 'EL3HLT', 47: 'EL3RST', 48: 'ELNRNG', 49: 'EUNATCH', 50: 'ENOCSI', 51: 'EL2HLT', 52: 'EBADE', 53: 'EBADR', 54: 'EXFULL', 55: 'ENOANO', 56: 'EBADRQC', 57: 'EBADSLT', 59: 'EBFONT', 60: 'ENOSTR', 61: 'ENODATA', 62: 'ETIME', 63: 'ENOSR', 64: 'ENONET', 65: 'ENOPKG', 66: 'EREMOTE', 67: 'ENOLINK', 68: 'EADV', 69: 'ESRMNT', 70: 'ECOMM', 71: 'EPROTO', 72: 'EMULTIHOP', 73: 'EDOTDOT', 74: 'EBADMSG', 75: 'EOVERFLOW', 76: 'ENOTUNIQ', 77: 'EBADFD', 78: 'EREMCHG', 79: 'ELIBACC', 80: 'ELIBBAD', 81: 'ELIBSCN', 82: 'ELIBMAX', 83: 'ELIBEXEC', 84: 'EILSEQ', 85: 'ERESTART', 86: 'ESTRPIPE', 87: 'EUSERS', 88: 'ENOTSOCK', 89: 'EDESTADDRREQ', 90: 'EMSGSIZE', 91: 'EPROTOTYPE', 92: 'ENOPROTOOPT', 93: 'EPROTONOSUPPORT', 94: 'ESOCKTNOSUPPORT', 95: 'ENOTSUP', 96: 'EPFNOSUPPORT', 97: 'EAFNOSUPPORT', 98: 'EADDRINUSE', 99: 'EADDRNOTAVAIL', 100: 'ENETDOWN', 101: 'ENETUNREACH', 102: 'ENETRESET', 103: 'ECONNABORTED', 104: 'ECONNRESET', 105: 'ENOBUFS', 106: 'EISCONN', 107: 'ENOTCONN', 108: 'ESHUTDOWN', 109: 'ETOOMANYREFS', 110: 'ETIMEDOUT', 111: 'ECONNREFUSED', 112: 'EHOSTDOWN', 113: 'EHOSTUNREACH', 114: 'EALREADY', 115: 'EINPROGRESS', 116: 'ESTALE', 117: 'EUCLEAN', 118: 'ENOTNAM', 119: 'ENAVAIL', 120: 'EISNAM', 121: 'EREMOTEIO', 122: 'EDQUOT', 123: 'ENOMEDIUM', 124: 'EMEDIUMTYPE', 125: 'ECANCELED', 126: 'ENOKEY', 127: 'EKEYEXPIRED', 128: 'EKEYREVOKED', 129: 'EKEYREJECTED', 130: 'EOWNERDEAD', 131: 'ENOTRECOVERABLE', 132: 'ERFKILL'}
@@ -15,10 +16,10 @@ linux_aliases = {'EOPNOTSUPP' : 'ENOTSUP', 'EDEADLK': 'EDEADLOCK', 'EWOULDBLOCK'
 darwin_aliases = {'EWOULDBLOCK': 'EAGAIN'}
 aliases = {**linux_aliases, **darwin_aliases}
 
-def set_value(errorval, name, value, index):
-    if name not in errorval:
-        errorval[name] = [None] * num_systems
-    errorval[name][index] = value
+def set_value(codeval, name, value, index):
+    if name not in codeval:
+        codeval[name] = [None] * len(systems)
+    codeval[name][index] = value
 
 def collect_errornames():
     errorval = {}
@@ -38,7 +39,7 @@ def collect_errornames():
 
     # set aliases
     for alias, target in aliases.items():
-        for idx in range(num_systems):
+        for idx in range(len(systems)):
             if errorval[target][idx] and not errorval[alias][idx]:
                 errorval[alias][idx] = errorval[target][idx]
 
@@ -57,7 +58,7 @@ def generate_errno_codes(cw):
         for code in codes:
             res *= 100000
             res += code if code else 0
-        return res + shifts * 100000 ** (num_systems - 1) // 10
+        return res + shifts * 100000 ** (len(systems) - 1) // 10
 
     names = sorted(errorvalues, key = priority)
     for name in names:
@@ -77,29 +78,29 @@ def generate_errno_codes(cw):
         if hidden_on:
             cw.write(f"[PythonHidden({', '.join(hidden_on)})]")
 
-        value = windows_code_expr(codes)
+        value = windows_code_expr(codes, str)
         cw.write(f"public static int {name} => {value};")
 
-def windows_code_expr(codes):
-    if codes[windows_idx]:
-        if not any(codes[:windows_idx]) or all(map(lambda x: x == codes[windows_idx], codes[:windows_idx])):
-            return str(codes[windows_idx])
+def windows_code_expr(codes, fmt):
+    if codes[windows_idx] is not None:
+        if all(c is None for c in codes[:windows_idx]) or all(x == codes[windows_idx] for x in codes[:windows_idx]):
+            return fmt(codes[windows_idx])
         else:
-            return f"RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? {codes[windows_idx]} : {darwin_code_expr(codes)}"
+            return f"RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? {fmt(codes[windows_idx])} : {darwin_code_expr(codes, fmt)}"
     else:
-        return darwin_code_expr(codes)
+        return darwin_code_expr(codes, fmt)
 
-def darwin_code_expr(codes):
-    if codes[darwin_idx]:
-        if not any(codes[:darwin_idx]) or all(map(lambda x: x == codes[darwin_idx], codes[:darwin_idx])):
-            return str(codes[darwin_idx])
+def darwin_code_expr(codes, fmt):
+    if codes[darwin_idx] is not None:
+        if all(c is None for c in codes[:darwin_idx]) or all(x == codes[darwin_idx] for x in codes[:darwin_idx]):
+            return fmt(codes[darwin_idx])
         else:
-            return f"RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? {codes[darwin_idx]} : {linux_code_expr(codes)}"
+            return f"RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? {fmt(codes[darwin_idx])} : {linux_code_expr(codes, fmt)}"
     else:
-        return linux_code_expr(codes)
+        return linux_code_expr(codes, fmt)
 
-def linux_code_expr(codes):
-    return str(codes[linux_idx])
+def linux_code_expr(codes, fmt):
+    return fmt(codes[linux_idx])
 
 def generate_errno_names(cw):
     def is_windows_alias(name):
@@ -142,10 +143,74 @@ def generate_errno_names(cw):
         cw.write(f'errorcode[{name}] = "{name}";')
     cw.exit_block()
 
+
+# python3 -c 'import os;print(dict(sorted((s, getattr(os, s)) for s in dir(os) if s.startswith("O_"))))'
+O_flags_linux = {'O_ACCMODE': 3, 'O_APPEND': 1024, 'O_ASYNC': 8192, 'O_CLOEXEC': 524288, 'O_CREAT': 64, 'O_DIRECT': 16384, 'O_DIRECTORY': 65536, 'O_DSYNC': 4096, 'O_EXCL': 128, 'O_FSYNC': 1052672, 'O_LARGEFILE': 0, 'O_NDELAY': 2048, 'O_NOATIME': 262144, 'O_NOCTTY': 256, 'O_NOFOLLOW': 131072, 'O_NONBLOCK': 2048, 'O_PATH': 2097152, 'O_RDONLY': 0, 'O_RDWR': 2, 'O_RSYNC': 1052672, 'O_SYNC': 1052672, 'O_TMPFILE': 4259840, 'O_TRUNC': 512, 'O_WRONLY': 1}
+O_flags_darwin = {'O_ACCMODE': 3, 'O_APPEND': 8, 'O_ASYNC': 64, 'O_CLOEXEC': 16777216, 'O_CREAT': 512, 'O_DIRECTORY': 1048576, 'O_DSYNC': 4194304, 'O_EXCL': 2048, 'O_EXEC': 1073741824, 'O_EXLOCK': 32, 'O_NDELAY': 4, 'O_NOCTTY': 131072, 'O_NOFOLLOW': 256, 'O_NONBLOCK': 4, 'O_RDONLY': 0, 'O_RDWR': 2, 'O_SEARCH': 1074790400, 'O_SHLOCK': 16, 'O_SYNC': 128, 'O_TRUNC': 1024, 'O_WRONLY': 1}
+O_flags_windows = {'O_APPEND': 8, 'O_BINARY': 32768, 'O_CREAT': 256, 'O_EXCL': 1024, 'O_NOINHERIT': 128, 'O_RANDOM': 16, 'O_RDONLY': 0, 'O_RDWR': 2, 'O_SEQUENTIAL': 32, 'O_SHORT_LIVED': 4096, 'O_TEMPORARY': 64, 'O_TEXT': 16384, 'O_TRUNC': 512, 'O_WRONLY': 1}
+
+O_flags_optional = {'O_ASYNC', 'O_DIRECT', 'O_DIRECTORY', 'O_NOFOLLOW', 'O_NOATIME', 'O_PATH', 'O_TMPFILE', 'O_SHLOCK', 'O_EXLOCK'}
+O_flags_python3_10 = {'O_EVTONLY', 'O_FSYNC', 'O_SYMLINK', 'O_NOFOLLOW_ANY'}
+
+def collect_codes():
+    codeval = {}
+    for name in O_flags_linux:
+        set_value(codeval, name, O_flags_linux[name], linux_idx)
+    for name in O_flags_darwin:
+        set_value(codeval, name, O_flags_darwin[name], darwin_idx)
+    for name in O_flags_windows:
+        set_value(codeval, name, O_flags_windows[name], windows_idx)
+    for name in O_flags_optional | O_flags_python3_10:
+        codeval.pop(name, None)
+    return OrderedDict(sorted(codeval.items()))
+
+O_flagvalues = collect_codes()
+
+def generate_O_flags(cw, flagvalues, access):
+    for name in flagvalues.keys():
+        codes = flagvalues[name]
+        hidden_on = []
+        supported_on = set(systems)
+        cw.writeline()
+        if codes[windows_idx] is None:
+            hidden_on = ["PlatformsAttribute.PlatformFamily.Windows"]
+            supported_on.discard(systems[windows_idx])
+        if codes[linux_idx] is None and codes[darwin_idx] is None:
+            assert not hidden_on, "Cannot hide on both Unix and Windows"
+            hidden_on = ["PlatformsAttribute.PlatformFamily.Unix"]
+            supported_on.discard(systems[linux_idx])
+            supported_on.discard(systems[darwin_idx])
+        else:
+            if codes[linux_idx] is None:
+                hidden_on += ["PlatformID.Unix"]
+                supported_on.discard(systems[linux_idx])
+            if codes[darwin_idx] is None:
+                hidden_on += ["PlatformID.MacOSX"]
+                supported_on.discard(systems[darwin_idx])
+        if hidden_on:
+            cw.write(f"[PythonHidden({', '.join(hidden_on)})]")
+        if len(supported_on) != len(systems):
+            for s in sorted(supported_on):
+                cw.write(f'[SupportedOSPlatform("{s}")]')
+
+        value = windows_code_expr(codes, fmt=hex)
+        cw.write(f"{access} static int {name} => {value};")
+
+def generate_all_O_flags(cw):
+    generate_O_flags(cw, O_flagvalues, 'public')
+
+common_O_flags = ['O_RDONLY', 'O_WRONLY', 'O_RDWR', 'O_APPEND', 'O_CREAT', 'O_TRUNC', 'O_EXCL']
+
+def generate_common_O_flags(cw):
+    generate_O_flags(cw, OrderedDict((f, O_flagvalues[f]) for f in common_O_flags), 'private')
+
+
 def main():
     return generate(
         ("Errno Codes", generate_errno_codes),
         ("Errno Names", generate_errno_names),
+        ("O_Flags", generate_all_O_flags),
+        ("Common O_Flags", generate_common_O_flags),
     )
 
 if __name__ == "__main__":

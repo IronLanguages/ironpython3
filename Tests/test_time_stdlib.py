e42cefb6b1128101dc59882669fde6a817cb55c1
Stéphane Lozier <slozier@users.noreply.github.com>
2023-08-24 21:53:45 -0400

    Add clocks

--- Tests/test_time_stdlib.py
+++ Tests/test_time_stdlib.py
@@ -20,13 +20,9 @@ def load_tests(loader, standard_tests, pattern):
             test.test_time.TestAsctime4dyear('test_negative'), # ValueError: year is too low
             test.test_time.TimeTestCase('test_asctime'), # ValueError: year is too high
             test.test_time.TimeTestCase('test_asctime_bounding_check'), # ValueError: Hour, Minute, and Second parameters describe an un-representable DateTime.
-            test.test_time.TimeTestCase('test_clock'), # NotImplementedError: get_clock_info('clock')
-            test.test_time.TimeTestCase('test_get_clock_info'), # NotImplementedError: get_clock_info
             test.test_time.TimeTestCase('test_insane_timestamps'), # ValueError: unreasonable date/time
             test.test_time.TimeTestCase('test_mktime_error'), # ValueError: year is too low
-            test.test_time.TimeTestCase('test_process_time'), # AttributeError: 'module' object has no attribute 'process_time'
             test.test_time.TimeTestCase('test_strftime_bounding_check'), # ValueError: Hour, Minute, and Second parameters describe an un-representable DateTime.
-            test.test_time.TimeTestCase('test_time'), # NotImplementedError: get_clock_info('time')
             test.test_time.TimeTestCase('test_default_values_for_zero'), # AssertionError: '2000 01 01 00 00 00 1 001' != '2000 01 01 00 00 00 6 001'
         ]
 

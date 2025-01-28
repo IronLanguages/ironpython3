405e246d8f2221fd053bd86f734cce43215c78a6
Pavel Koneski <pavel.koneski@gmail.com>
2025-01-27 21:13:57 -0800

    Skip `pty`-dependent tests on Mono

--- tests/suite/test_builtin_stdlib.py
+++ tests/suite/test_builtin_stdlib.py
@@ -23,15 +23,16 @@ def load_tests(loader, standard_tests, pattern):
             test.test_builtin.BuiltinTest('test_open_non_inheritable'), # https://github.com/IronLanguages/ironpython3/issues/1225
         ]
 
-        skip_tests = []
+        skip_tests = [
+            # module `pty` is importable but not functional on .NET Core
+            test.test_builtin.PtyTests('test_input_no_stdout_fileno'),
+            test.test_builtin.PtyTests('test_input_tty'),
+            test.test_builtin.PtyTests('test_input_tty_non_ascii'),
+            test.test_builtin.PtyTests('test_input_tty_non_ascii_unicode_errors'),
+            test.test_builtin.ShutdownTest('test_cleanup'),
+        ]
         if is_netcoreapp:
             skip_tests += [
-                # module `pty` is importable but not functional on .NET Core
-                test.test_builtin.PtyTests('test_input_no_stdout_fileno'),
-                test.test_builtin.PtyTests('test_input_tty'),
-                test.test_builtin.PtyTests('test_input_tty_non_ascii'),
-                test.test_builtin.PtyTests('test_input_tty_non_ascii_unicode_errors'),
-                test.test_builtin.ShutdownTest('test_cleanup'),
             ]
 
         return generate_suite(tests, failing_tests, skip_tests)

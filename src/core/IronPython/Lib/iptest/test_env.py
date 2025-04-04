07c4ca7e79b2ef58e448c8a948e7b8d74872ec11
Pavel Koneski <pavel.koneski@gmail.com>
2025-04-04 16:10:48 -0700

    Chanke machine test to be cross-platform

--- src/core/IronPython/Lib/iptest/test_env.py
+++ src/core/IronPython/Lib/iptest/test_env.py
@@ -4,6 +4,7 @@
 
 import os
 import sys
+import platform
 
 #------------------------------------------------------------------------------
 
@@ -15,7 +16,7 @@ is_windows    =  sys.platform == 'win32'
 is_linux      =  sys.platform == 'linux'
 is_osx        =  sys.platform == 'darwin'
 is_posix      =  is_linux or is_osx
-is_arm64      =  os.uname().machine in ['aarch64', 'arm64']
+is_arm64      =  platform.machine().lower() in ['aarch64', 'arm64']
 
 is_netcoreapp =  False
 is_netcoreapp21 = False

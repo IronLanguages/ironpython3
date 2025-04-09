f9e8f64491e13171002a357e798b6b549e830689
Pavel Koneski <pavel.koneski@gmail.com>
2025-04-09 14:34:14 -0700

    Make test_signal passing with CPython/macOS

--- tests/suite/modules/system_related/test_signal.py
+++ tests/suite/modules/system_related/test_signal.py
@@ -11,109 +11,131 @@ TODO: until we can send signals to other processes consistently (e.g., os.kill),
 import signal
 import sys
 
-from iptest import IronPythonTestCase, is_cli, run_test
+from iptest import IronPythonTestCase, is_windows, is_posix, is_osx, is_linux, run_test
 
-SUPPORTED_SIGNALS = [   signal.SIGBREAK,
-                        signal.SIGABRT,
-                        signal.SIGINT,
-                        signal.SIGTERM,
-                        signal.SIGFPE,
-                        signal.SIGILL,
-                        signal.SIGSEGV,
-                        ]
+if is_linux:
+    SIG_codes = {'SIGABRT': 6, 'SIGALRM': 14, 'SIGBUS': 7, 'SIGCHLD': 17, 'SIGCLD': 17, 'SIGCONT': 18, 'SIGFPE': 8, 'SIGHUP': 1, 'SIGILL': 4, 'SIGINT': 2, 'SIGIO': 29, 'SIGIOT': 6, 'SIGKILL': 9, 'SIGPIPE': 13, 'SIGPOLL': 29, 'SIGPROF': 27, 'SIGPWR': 30, 'SIGQUIT': 3, 'SIGRTMAX': 64, 'SIGRTMIN': 34, 'SIGSEGV': 11, 'SIGSTKFLT': 16, 'SIGSTOP': 19, 'SIGSYS': 31, 'SIGTERM': 15, 'SIGTRAP': 5, 'SIGTSTP': 20, 'SIGTTIN': 21, 'SIGTTOU': 22, 'SIGURG': 23, 'SIGUSR1': 10, 'SIGUSR2': 12, 'SIGVTALRM': 26, 'SIGWINCH': 28, 'SIGXCPU': 24, 'SIGXFSZ': 25}
+elif is_osx:
+    SIG_codes = {'SIGABRT': 6, 'SIGALRM': 14, 'SIGBUS': 10, 'SIGCHLD': 20, 'SIGCONT': 19, 'SIGEMT': 7, 'SIGFPE': 8, 'SIGHUP': 1, 'SIGILL': 4, 'SIGINFO': 29, 'SIGINT': 2, 'SIGIO': 23, 'SIGIOT': 6, 'SIGKILL': 9, 'SIGPIPE': 13, 'SIGPROF': 27, 'SIGQUIT': 3, 'SIGSEGV': 11, 'SIGSTOP': 17, 'SIGSYS': 12, 'SIGTERM': 15, 'SIGTRAP': 5, 'SIGTSTP': 18, 'SIGTTIN': 21, 'SIGTTOU': 22, 'SIGURG': 16, 'SIGUSR1': 30, 'SIGUSR2': 31, 'SIGVTALRM': 26, 'SIGWINCH': 28, 'SIGXCPU': 24, 'SIGXFSZ': 25}
+elif is_windows:
+    SIG_codes = {'SIGABRT': 22, 'SIGBREAK': 21, 'SIGFPE': 8, 'SIGILL': 4, 'SIGINT': 2, 'SIGSEGV': 11, 'SIGTERM': 15}
+
+SUPPORTED_SIGNALS = set(SIG_codes.values())
 
 class SignalTest(IronPythonTestCase):
 
     def test_000_run_me_first(self):
-        for x in [x for x in SUPPORTED_SIGNALS if x!=signal.SIGINT]:
-            self.assertEqual(signal.getsignal(x), 0)
-        self.assertEqual(signal.getsignal(signal.SIGINT), signal.default_int_handler)
+        WEIRD_CASES = { signal.SIGINT: signal.default_int_handler }
+        if is_posix:
+            WEIRD_CASES[signal.SIGKILL] = None
+            WEIRD_CASES[signal.SIGSTOP] = None
+            WEIRD_CASES[signal.SIGPIPE] = signal.SIG_IGN
+            WEIRD_CASES[signal.SIGXFSZ] = signal.SIG_IGN
 
-        for x in range(1, 23):
+        for x in [x for x in SUPPORTED_SIGNALS]:
+            with self.subTest(sig=x):
+                self.assertEqual(signal.getsignal(x), WEIRD_CASES.get(x, signal.SIG_DFL))
+
+        for x in range(1, signal.NSIG):
             if x in SUPPORTED_SIGNALS: continue
             self.assertEqual(signal.getsignal(x), None)
 
+
     def test_signal_getsignal_negative(self):
-        for x in [-2, -1, 0, 23, 24, 25]:
+        for x in [-2, -1, 0, signal.NSIG, signal.NSIG + 1, signal.NSIG + 2]:
             self.assertRaisesMessage(ValueError, "signal number out of range",
                                 signal.getsignal, x)
         for x in [None, "abc", "14"]:
             self.assertRaises(TypeError, signal.getsignal, x)
 
+
     def test_module_constants(self):
-        self.assertEqual(signal.NSIG, 23)
-        self.assertEqual(signal.SIGABRT, 22)
-        self.assertEqual(signal.SIGBREAK, 21)
-        self.assertEqual(signal.SIGFPE, 8)
-        self.assertEqual(signal.SIGILL, 4)
-        self.assertEqual(signal.SIGINT, 2)
-        self.assertEqual(signal.SIGSEGV, 11)
-        self.assertEqual(signal.SIGTERM, 15)
-        self.assertEqual(signal.SIG_DFL, 0)
-        self.assertEqual(signal.SIG_IGN, 1)
+        if is_linux:
+            self.assertEqual(signal.NSIG, 65)
+        elif is_osx:
+            self.assertEqual(signal.NSIG, 32)
+        elif is_windows:
+            self.assertEqual(signal.NSIG, 23)
+
+        # when run with CPython, this verifies that SIG_codes are correct and matching CPython
+        for sig in SIG_codes:
+            self.assertEqual(getattr(signal, sig), SIG_codes[sig])
+
 
     def test_doc(self):
+        signal_module = signal
         if sys.version_info >= (3,5):
-            self.assertIsNone(signal.__doc__)
-        else:
-            self.assertTrue("get the signal action for a given signal" in signal.__doc__)
+            import _signal
+            signal_module = _signal
+        self.assertTrue("get the signal action for a given signal" in signal_module.__doc__)
         self.assertTrue("The default handler for SIGINT installed by Python" in signal.default_int_handler.__doc__)
         self.assertTrue("Return the current action for the given signal" in signal.getsignal.__doc__)
         self.assertTrue("Set the action for the given signal" in signal.signal.__doc__)
+
 
     def test_signal_signal_neg(self):
         def a(b, c):
             pass
 
-        WEIRD_WORKING_CASES = [6] if is_cli else []
-        NO_SUCH_DIR = [21]
-
-        for x in range(1, 23):
+        for x in range(1, signal.NSIG):
             if x in SUPPORTED_SIGNALS: continue
-            if x in WEIRD_WORKING_CASES: continue
-            self.assertRaises(RuntimeError if is_cli else ValueError,
+            self.assertRaises(ValueError,
                         signal.signal, x, a)
 
-        for x in [-2, -1, 0, 23, 24, 25]:
-            self.assertRaisesMessage(ValueError, "signal number out of range" if is_cli else "invalid signal value",
+        for x in [-2, -1, 0, signal.NSIG, signal.NSIG + 1, signal.NSIG + 2]:
+            self.assertRaisesMessage(ValueError, "signal number out of range",
                                 signal.signal, x, a)
 
         def bad_sig0(): pass
         def bad_sig1(a): pass
         def bad_sig3(a,b,c): pass
 
-        for y in SUPPORTED_SIGNALS + WEIRD_WORKING_CASES:
+        for y in SUPPORTED_SIGNALS:
             bad_handlers = [-2, -1, 2, 3, 4, 10, 22, 23, 24, None]
             for x in bad_handlers:
                 self.assertRaisesMessage(TypeError, "signal handler must be signal.SIG_IGN, signal.SIG_DFL, or a callable object",
                                     signal.signal, y, x)
 
+
     def test_signal_signal(self):
-        WORKING_CASES = SUPPORTED_SIGNALS
-        if is_cli:
-            WORKING_CASES += [6]
-        WEIRD_CASES = {
-                    6: None,
-                    2: signal.default_int_handler}
-        for x in WORKING_CASES:
-            #Ideal handler signature
-            def a(signum, frame):
-                return x
+        WEIRD_CASES = { signal.SIGINT: signal.default_int_handler}
+        if is_posix:
+            WEIRD_CASES[signal.SIGKILL] = None
+            WEIRD_CASES[signal.SIGSTOP] = None
+            WEIRD_CASES[signal.SIGPIPE] = signal.SIG_IGN
+            WEIRD_CASES[signal.SIGXFSZ] = signal.SIG_IGN
 
-            ret_val = signal.signal(x, a)
-            if x not in WEIRD_CASES.keys():
-                self.assertEqual(ret_val, signal.SIG_DFL)
-            else:
-                self.assertEqual(ret_val, WEIRD_CASES[x])
-            self.assertEqual(a, signal.getsignal(x))
+        for x in SUPPORTED_SIGNALS:
+            with self.subTest(sig=x):
 
-        #Strange handler signatures
+                # correctness of expected handler tested by assertion in test_000_run_me_first
+                expected_val = WEIRD_CASES.get(x, signal.SIG_DFL)
+
+                if expected_val is None:
+                    # signals that cannot be handled
+                    with self.assertRaises(OSError) as cm:
+                        signal.signal(x, signal.SIG_DFL)
+
+                    self.assertEqual(cm.exception.errno, 22)
+                    self.assertEqual(cm.exception.strerror, "Invalid argument")
+                else:
+                    # Ideal handler signature
+                    def a(signum, frame):
+                        return x
+
+                    ret_val = signal.signal(x, a)
+                    self.assertEqual(ret_val, expected_val)
+                    self.assertEqual(a, signal.getsignal(x))
+                    self.assertEqual(a, signal.signal(x, expected_val))  # restores old handler
+
+        # Strange handler signatures
         class KNew(object):
             def __call__(self, *args, **kwargs):
                 pass
 
         a = KNew()
-        ret_val = signal.signal(signal.SIGBREAK, a)
-        self.assertEqual(a, signal.getsignal(signal.SIGBREAK))
+        ret_val = signal.signal(signal.SIGINT, a)
+        self.assertEqual(a, signal.getsignal(signal.SIGINT))
+
 
 run_test(__name__)

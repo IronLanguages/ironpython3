33a8b156bc2a061b1530362e6427f64ddcd47aa3
slozier <slozier@users.noreply.github.com>
2025-07-12 12:50:05 -0400

    Add stub for _winapi.SetNamedPipeHandleState (#1963)

---
+++ src/core/IronPython/Interop/Windows/Kernel32/Interop.SetNamedPipeHandleState.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.Win32.SafeHandles;
+
+using System;
+using System.Runtime.InteropServices;
+
+internal static partial class Interop {
+    internal static partial class Kernel32 {
+        [DllImport(Libraries.Kernel32, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern unsafe bool SetNamedPipeHandleState(
+          SafePipeHandle hNamedPipe,
+          ref int lpMode,
+          IntPtr lpMaxCollectionCount,
+          IntPtr lpCollectDataTimeout
+        );
+    }
+}

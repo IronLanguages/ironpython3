4df6d1d0d73ef3101cbe72bdc69713bd65ab5484
Stéphane Lozier <slozier@users.noreply.github.com>
2026-02-23 21:46:17 -0500

    Fix test_socket_stdlib

--- tests/suite/test_socket_stdlib.py
+++ tests/suite/test_socket_stdlib.py
@@ -41,7 +41,7 @@ def load_tests(loader, standard_tests, pattern):
             failing_tests += [
                 test.test_socket.NonBlockingTCPTests('testRecv'), # TODO: figure out
             ]
-        else:
+        if not is_mono:
             failing_tests += [
                 test.test_socket.GeneralModuleTests('test_getnameinfo'), # https://github.com/IronLanguages/ironpython3/issues/1222
             ]

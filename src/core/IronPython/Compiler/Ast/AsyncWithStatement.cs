afb63166ac8bb739a7513fc01a4b05dcf32b0e8d
slozier <slozier@users.noreply.github.com>
2026-03-14 10:21:40 -0400

    Cleanup AsyncWithStatement and AsyncForStatement (#2022)

--- src/core/IronPython/Compiler/Ast/AsyncWithStatement.cs
+++ src/core/IronPython/Compiler/Ast/AsyncWithStatement.cs
@@ -52,57 +52,42 @@ namespace IronPython.Compiler.Ast {
             // finally:
             //     await mgr.__aexit__(None, None, None)
 
-            // Helper to create nodes with proper parent and span
-            NameExpression MakeName(string name) {
-                var n = new NameExpression(name) { Parent = parent };
-                n.IndexSpan = span;
-                return n;
+            // Helper to assign proper parent and span to nodes
+            T SetScope<T>(T node) where T : Node {
+                node.Parent = parent;
+                node.IndexSpan = span;
+                return node;
             }
 
             // mgr = EXPR
-            var assignMgr = new AssignmentStatement(new Expression[] { MakeName("__asyncwith_mgr") }, ContextManager) { Parent = parent };
-            assignMgr.IndexSpan = span;
+            var assignMgr = SetScope(new AssignmentStatement([SetScope(new NameExpression("__asyncwith_mgr"))], ContextManager));
 
             // await mgr.__aenter__()
-            var aenterAttr = new MemberExpression(MakeName("__asyncwith_mgr"), "__aenter__") { Parent = parent };
-            aenterAttr.IndexSpan = span;
-            var aenterCall = new CallExpression(aenterAttr, null, null) { Parent = parent };
-            aenterCall.IndexSpan = span;
+            var aenterAttr = SetScope(new MemberExpression(SetScope(new NameExpression("__asyncwith_mgr")), "__aenter__"));
+            var aenterCall = SetScope(new CallExpression(aenterAttr, null, null));
             var awaitEnter = new AwaitExpression(aenterCall);
 
             Statement bodyStmt;
-            if (Variable != null) {
+            if (Variable is not null) {
                 // VAR = await value; BLOCK
-                var assignVar = new AssignmentStatement(new Expression[] { Variable }, awaitEnter) { Parent = parent };
-                assignVar.IndexSpan = span;
-                bodyStmt = new SuiteStatement(new Statement[] { assignVar, Body }) { Parent = parent };
+                var assignVar = SetScope(new AssignmentStatement([Variable], awaitEnter));
+                bodyStmt = new SuiteStatement([assignVar, Body]) { Parent = parent };
             } else {
-                var exprStmt = new ExpressionStatement(awaitEnter) { Parent = parent };
-                exprStmt.IndexSpan = span;
-                bodyStmt = new SuiteStatement(new Statement[] { exprStmt, Body }) { Parent = parent };
+                var exprStmt = SetScope(new ExpressionStatement(awaitEnter));
+                bodyStmt = new SuiteStatement([exprStmt, Body]) { Parent = parent };
             }
 
             // await mgr.__aexit__(None, None, None)
-            var aexitAttr = new MemberExpression(MakeName("__asyncwith_mgr"), "__aexit__") { Parent = parent };
-            aexitAttr.IndexSpan = span;
-            var none1 = new ConstantExpression(null) { Parent = parent }; none1.IndexSpan = span;
-            var none2 = new ConstantExpression(null) { Parent = parent }; none2.IndexSpan = span;
-            var none3 = new ConstantExpression(null) { Parent = parent }; none3.IndexSpan = span;
-            var aexitCallNormal = new CallExpression(aexitAttr,
-                new Expression[] { none1, none2, none3 }, null) { Parent = parent };
-            aexitCallNormal.IndexSpan = span;
+            var aexitAttr = SetScope(new MemberExpression(SetScope(new NameExpression("__asyncwith_mgr")), "__aexit__"));
+            var none = SetScope(new ConstantExpression(null));
+            var aexitCallNormal = SetScope(new CallExpression(aexitAttr, [none, none, none], null));
             var awaitExitNormal = new AwaitExpression(aexitCallNormal);
 
             // try/finally: await __aexit__ on normal exit
-            var finallyExprStmt = new ExpressionStatement(awaitExitNormal) { Parent = parent };
-            finallyExprStmt.IndexSpan = span;
-            var tryFinally = new TryStatement(bodyStmt, null, null, finallyExprStmt) { Parent = parent };
-            tryFinally.IndexSpan = span;
-            tryFinally.HeaderIndex = span.End;
+            var finallyExprStmt = SetScope(new ExpressionStatement(awaitExitNormal));
+            var tryFinally = SetScope(new TryStatement(bodyStmt, null, null, finallyExprStmt) { HeaderIndex = span.End });
 
-            var suite = new SuiteStatement(new Statement[] { assignMgr, tryFinally }) { Parent = parent };
-            suite.IndexSpan = span;
-            return suite;
+            return SetScope(new SuiteStatement([assignMgr, tryFinally]));
         }
 
         public override MSAst.Expression Reduce() {
